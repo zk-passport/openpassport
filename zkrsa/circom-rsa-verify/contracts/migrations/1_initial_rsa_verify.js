3b7c0375df5c23727ea5fd33a7c83d209581d37e
0xturboblitz <florent.tavernier@gmail.com>
2023-07-22 15:23:00 +0200

    sol verifier and fixed scripts in package.json

--- zkrsa/circom-rsa-verify/contracts/migrations/1_initial_rsa_verify.js
+++ zkrsa/circom-rsa-verify/contracts/migrations/1_initial_rsa_verify.js
@@ -1,12 +1,14 @@
-const RsaVerify = artifacts.require("RsaVerify");
+// const RsaVerify = artifacts.require("RsaVerify");
+const Verifier = artifacts.require("Verifier");
 
 async function doDeploy(deployer) {
-  await deployer.deploy(RsaVerify);
+  // await deployer.deploy(RsaVerify);
+  await deployer.deploy(Verifier);
 }
-
 
 module.exports = (deployer) => {
   deployer.then(async () => {
       await doDeploy(deployer);
   });
 };
+
