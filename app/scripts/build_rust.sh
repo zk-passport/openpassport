81a29d63d967fcf09791c24535a0e74ad8d4fb22
0xturboblitz <florent.tavernier@gmail.com>
2023-11-04 22:44:06 +0300

    just some config

--- app/scripts/build_rust.sh
+++ app/scripts/build_rust.sh
@@ -6,12 +6,12 @@ cd ../android
 cd ..
 
 mkdir -p android/react-native-passport-reader/android/src/main/jniLibs/arm64/
-if [ -f "ark-circom-passport/target/aarch64-linux-android/release/libark_circom_passport.so" ]; then
+# if [ -f "ark-circom-passport/target/aarch64-linux-android/release/libark_circom_passport.so" ]; then
   echo copied release version
   cp ark-circom-passport/target/aarch64-linux-android/release/libark_circom_passport.so android/react-native-passport-reader/android/src/main/jniLibs/arm64/
-elif [ -f "ark-circom-passport/target/aarch64-linux-android/debug/libark_circom_passport.so" ]; then
-  echo copied debug version
-  cp ark-circom-passport/target/aarch64-linux-android/debug/libark_circom_passport.so android/react-native-passport-reader/android/src/main/jniLibs/arm64/
-fi
+# elif [ -f "ark-circom-passport/target/aarch64-linux-android/debug/libark_circom_passport.so" ]; then
+  # echo copied debug version
+  # cp ark-circom-passport/target/aarch64-linux-android/debug/libark_circom_passport.so android/react-native-passport-reader/android/src/main/jniLibs/arm64/
+# fi
 
 
