cf546aebfe18a052d81eb9b3955e49e4c2edf712
0xturboblitz <florent.tavernier@gmail.com>
2023-11-26 12:36:05 +0100

    proof of passport nft

---
+++ contracts/contracts/Verifier.sol
@@ -0,0 +1,205 @@
+// SPDX-License-Identifier: GPL-3.0
+/*
+    Copyright 2021 0KIMS association.
+
+    This file is generated with [snarkJS](https://github.com/iden3/snarkjs).
+
+    snarkJS is a free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    snarkJS is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with snarkJS. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Groth16Verifier {
+    // Scalar field size
+    uint256 constant r    = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
+    // Base field size
+    uint256 constant q   = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+
+    // Verification Key data
+    uint256 constant alphax  = 20491192805390485299153009773594534940189261866228447918068658471970481763042;
+    uint256 constant alphay  = 9383485363053290200918347156157836566562967994039712273449902621266178545958;
+    uint256 constant betax1  = 4252822878758300859123897981450591353533073413197771768651442665752259397132;
+    uint256 constant betax2  = 6375614351688725206403948262868962793625744043794305715222011528459656738731;
+    uint256 constant betay1  = 21847035105528745403288232691147584728191162732299865338377159692350059136679;
+    uint256 constant betay2  = 10505242626370262277552901082094356697409835680220590971873171140371331206856;
+    uint256 constant gammax1 = 11559732032986387107991004021392285783925812861821192530917403151452391805634;
+    uint256 constant gammax2 = 10857046999023057135944570762232829481370756359578518086990519993285655852781;
+    uint256 constant gammay1 = 4082367875863433681332203403145435568316851327593401208105741076214120093531;
+    uint256 constant gammay2 = 8495653923123431417604973247489272438418190587263600148770280649306958101930;
+    uint256 constant deltax1 = 10880293230004982776202427122285134624991864544958475343927602874213575641554;
+    uint256 constant deltax2 = 16624665484234285310515487394475180399116885285129839531968705310830253875556;
+    uint256 constant deltay1 = 3613064115835683935137165261501292410418255296432922653635183266285874098430;
+    uint256 constant deltay2 = 19291133931644078725883864430591991945789379570701252372126187841104950813463;
+
+    
+    uint256 constant IC0x = 5273738681235489626795253249156042954686324696253697750333394380857132417839;
+    uint256 constant IC0y = 9857192952041840089161633994514015537135554971079134835409588292482738477181;
+    
+    uint256 constant IC1x = 13099273046850541163676426713665613850498239380428162716631655985866490422114;
+    uint256 constant IC1y = 805402081419020794565351139094125407243790898682279460683021914187983510021;
+    
+    uint256 constant IC2x = 4265450637747300781024965299586779540792520368072350700653387749302516246471;
+    uint256 constant IC2y = 7463843387891834357375737290177319173209344475125750726872035788722899299932;
+    
+    uint256 constant IC3x = 20088836232532557466758901319787909748432281529666059108643164527823125804789;
+    uint256 constant IC3y = 16147539042430614121465862556890947026732365730825643639125269303010476393164;
+    
+    uint256 constant IC4x = 24929856770016377874928422014228270742044953121105820213504610769400107305;
+    uint256 constant IC4y = 19728513400524546085926773844123831498204802391551889897087063418483261808497;
+    
+    uint256 constant IC5x = 16348551320150140412227080661010174367526100186688295471629946307478569964727;
+    uint256 constant IC5y = 20290740830545611972538939670712192944691799477169965616578940279198383183813;
+    
+    uint256 constant IC6x = 6054573107363011273023497115562598583444742570417299435209185300867053768320;
+    uint256 constant IC6y = 10299856535915777925194272650832527282284880643409307207104854021775505827551;
+    
+ 
+    // Memory data
+    uint16 constant pVk = 0;
+    uint16 constant pPairing = 128;
+
+    uint16 constant pLastMem = 896;
+
+    function verifyProof(uint[2] calldata _pA, uint[2][2] calldata _pB, uint[2] calldata _pC, uint[6] calldata _pubSignals) public view returns (bool) {
+        assembly {
+            function checkField(v) {
+                if iszero(lt(v, q)) {
+                    mstore(0, 0)
+                    return(0, 0x20)
+                }
+            }
+            
+            // G1 function to multiply a G1 value(x,y) to value in an address
+            function g1_mulAccC(pR, x, y, s) {
+                let success
+                let mIn := mload(0x40)
+                mstore(mIn, x)
+                mstore(add(mIn, 32), y)
+                mstore(add(mIn, 64), s)
+
+                success := staticcall(sub(gas(), 2000), 7, mIn, 96, mIn, 64)
+
+                if iszero(success) {
+                    mstore(0, 0)
+                    return(0, 0x20)
+                }
+
+                mstore(add(mIn, 64), mload(pR))
+                mstore(add(mIn, 96), mload(add(pR, 32)))
+
+                success := staticcall(sub(gas(), 2000), 6, mIn, 128, pR, 64)
+
+                if iszero(success) {
+                    mstore(0, 0)
+                    return(0, 0x20)
+                }
+            }
+
+            function checkPairing(pA, pB, pC, pubSignals, pMem) -> isOk {
+                let _pPairing := add(pMem, pPairing)
+                let _pVk := add(pMem, pVk)
+
+                mstore(_pVk, IC0x)
+                mstore(add(_pVk, 32), IC0y)
+
+                // Compute the linear combination vk_x
+                
+                g1_mulAccC(_pVk, IC1x, IC1y, calldataload(add(pubSignals, 0)))
+                
+                g1_mulAccC(_pVk, IC2x, IC2y, calldataload(add(pubSignals, 32)))
+                
+                g1_mulAccC(_pVk, IC3x, IC3y, calldataload(add(pubSignals, 64)))
+                
+                g1_mulAccC(_pVk, IC4x, IC4y, calldataload(add(pubSignals, 96)))
+                
+                g1_mulAccC(_pVk, IC5x, IC5y, calldataload(add(pubSignals, 128)))
+                
+                g1_mulAccC(_pVk, IC6x, IC6y, calldataload(add(pubSignals, 160)))
+                
+
+                // -A
+                mstore(_pPairing, calldataload(pA))
+                mstore(add(_pPairing, 32), mod(sub(q, calldataload(add(pA, 32))), q))
+
+                // B
+                mstore(add(_pPairing, 64), calldataload(pB))
+                mstore(add(_pPairing, 96), calldataload(add(pB, 32)))
+                mstore(add(_pPairing, 128), calldataload(add(pB, 64)))
+                mstore(add(_pPairing, 160), calldataload(add(pB, 96)))
+
+                // alpha1
+                mstore(add(_pPairing, 192), alphax)
+                mstore(add(_pPairing, 224), alphay)
+
+                // beta2
+                mstore(add(_pPairing, 256), betax1)
+                mstore(add(_pPairing, 288), betax2)
+                mstore(add(_pPairing, 320), betay1)
+                mstore(add(_pPairing, 352), betay2)
+
+                // vk_x
+                mstore(add(_pPairing, 384), mload(add(pMem, pVk)))
+                mstore(add(_pPairing, 416), mload(add(pMem, add(pVk, 32))))
+
+
+                // gamma2
+                mstore(add(_pPairing, 448), gammax1)
+                mstore(add(_pPairing, 480), gammax2)
+                mstore(add(_pPairing, 512), gammay1)
+                mstore(add(_pPairing, 544), gammay2)
+
+                // C
+                mstore(add(_pPairing, 576), calldataload(pC))
+                mstore(add(_pPairing, 608), calldataload(add(pC, 32)))
+
+                // delta2
+                mstore(add(_pPairing, 640), deltax1)
+                mstore(add(_pPairing, 672), deltax2)
+                mstore(add(_pPairing, 704), deltay1)
+                mstore(add(_pPairing, 736), deltay2)
+
+
+                let success := staticcall(sub(gas(), 2000), 8, _pPairing, 768, _pPairing, 0x20)
+
+                isOk := and(success, mload(_pPairing))
+            }
+
+            let pMem := mload(0x40)
+            mstore(0x40, add(pMem, pLastMem))
+
+            // Validate that all evaluations ∈ F
+            
+            checkField(calldataload(add(_pubSignals, 0)))
+            
+            checkField(calldataload(add(_pubSignals, 32)))
+            
+            checkField(calldataload(add(_pubSignals, 64)))
+            
+            checkField(calldataload(add(_pubSignals, 96)))
+            
+            checkField(calldataload(add(_pubSignals, 128)))
+            
+            checkField(calldataload(add(_pubSignals, 160)))
+            
+            checkField(calldataload(add(_pubSignals, 192)))
+            
+
+            // Validate all evaluations
+            let isValid := checkPairing(_pA, _pB, _pC, _pubSignals, pMem)
+
+            mstore(0, isValid)
+             return(0, 0x20)
+         }
+     }
+ }
--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -6,37 +6,12 @@ const config: HardhatUserConfig = {
   solidity: "0.8.9",
   defaultNetwork: "hardhat",
   networks: {
-    hardhat: {
-      chainId: process.env.HARDHAT_CHAIN_ID
-        ? Number(process.env.HARDHAT_CHAIN_ID)
-        : 31337,
-    },
     goerli: {
       url: "https://eth-goerli.public.blastapi.io",
       accounts: [process.env.PKEY as string],
     },
     polygon: {
       url: "https://polygon.llamarpc.com",
-      accounts: [process.env.PKEY as string],
-    },
-    gnosis: {
-      url: "https://rpc.chiado.gnosis.gateway.fm",
-      accounts: [process.env.PKEY as string],
-    },
-    linea: {
-      url: `https://linea-goerli.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PKEY as string],
-    },
-    mantle: {
-      url: "https://rpc.testnet.mantle.xyz",
-      accounts: [process.env.PKEY as string],
-    },
-    neon: {
-      url: "https://devnet.neonevm.org",
-      accounts: [process.env.PKEY as string],
-    },
-    celo: {
-      url: "https://alfajores-forno.celo-testnet.org",
       accounts: [process.env.PKEY as string],
     },
   },
