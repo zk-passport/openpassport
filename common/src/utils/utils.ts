4bd6c162ab653be27e45fdcc535d6a1e03a5e6ff
0xturboblitz <florent.tavernier@gmail.com>
2023-12-05 20:08:02 -0500

    commented out passport number length check

--- common/src/utils/utils.ts
+++ common/src/utils/utils.ts
@@ -110,9 +110,9 @@ export function checkInputs(
   dateOfBirth: string,
   dateOfExpiry: string,
 ): boolean {
-  if (passportNumber.length !== 9) {
-    throw new Error('Passport number must be 9 characters long');
-  }
+  // if (passportNumber.length !== 9) {
+  //   throw new Error('Passport number must be 9 characters long');
+  // }
   if (dateOfBirth.length !== 6) {
     throw new Error('Date of birth must be 6 characters long');
   }
