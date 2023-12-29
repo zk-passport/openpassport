4994b41addc81228f80e58d96b3d7962502debbc
0xturboblitz <florent.tavernier@gmail.com>
2023-12-30 00:08:20 +0100

    remove poseidon from circuit

--- circuits/README.md
+++ circuits/README.md
@@ -20,6 +20,12 @@ yarn
 ./scripts/build_circuit.sh
 ```
 
+#### Build only to use the app, not for running tests (dev only, not secure)
+
+```bash
+./scripts/build_circuit.sh app-only
+```
+
 #### Run tests
 
 ```bash
--- circuits/scripts/build_circuit.sh
+++ circuits/scripts/build_circuit.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+# Check if the first argument is "app-only"
+if [ "$1" == "app-only" ]; then
+    echo "Building only for the app"
+    APP_ONLY=1
+else
+    APP_ONLY=0
+fi
 
 mkdir -p build
 cd build    
@@ -13,6 +22,19 @@ cd ..
 echo "compiling circuit"
 circom circuits/proof_of_passport.circom --r1cs --sym --wasm --output build
 
+mkdir -p ../app/ark-circom-passport/passport/
+cp build/proof_of_passport.r1cs ../app/ark-circom-passport/passport/
+cp build/proof_of_passport_js/proof_of_passport.wasm ../app/ark-circom-passport/passport/
+echo "copied proof_of_passport.r1cs and proof_of_passport.wasm to ark-circom-passport"
+echo "file sizes:"
+echo "Size of proof_of_passport.r1cs: $(wc -c <../app/ark-circom-passport/passport/proof_of_passport.r1cs) bytes"
+echo "Size of proof_of_passport.wasm: $(wc -c <../app/ark-circom-passport/passport/proof_of_passport.wasm) bytes"
+
+# If APP_ONLY is 1, exit the script here
+if [ $APP_ONLY -eq 1 ]; then
+    exit 0
+fi
+
 echo "building zkey"
 yarn snarkjs groth16 setup build/proof_of_passport.r1cs build/powersOfTau28_hez_final_20.ptau build/proof_of_passport.zkey
 
@@ -21,3 +43,5 @@ echo "test random" | yarn snarkjs zkey contribute build/proof_of_passport.zkey b
 yarn snarkjs zkey export verificationkey build/proof_of_passport_final.zkey build/verification_key.json
 
 yarn snarkjs zkey export solidityverifier build/proof_of_passport_final.zkey build/Verifier.sol
+cp build/Verifier.sol ../contracts/contracts/Verifier.sol
+echo "copied Verifier.sol to contracts"
