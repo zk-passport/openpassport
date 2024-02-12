ff2ca33912a5d459ef975899edc4cad9394d6129
0xturboblitz <florent.tavernier@gmail.com>
2024-02-12 17:59:49 +0100

    fix build failure

--- app/index.js
+++ app/index.js
@@ -7,8 +7,7 @@ import App from './App';
 import { name as appName } from './app.json';
 import { TamaguiProvider } from 'tamagui';
 import { createTamagui } from 'tamagui';
-import { config } from '@tamagui/config/v3'
-
+import { config } from '@tamagui/config/v2-native'
 const tamaguiConfig = createTamagui(config)
 
 
