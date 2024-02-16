4cd01c5d3f469e2817c0146c4981e871dd706f94
0xturboblitz <florent.tavernier@gmail.com>
2024-02-16 16:33:34 +0100

    fix react-native-reanimated bug

--- app/babel.config.js
+++ app/babel.config.js
@@ -2,5 +2,6 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     ["@babel/plugin-transform-private-methods", { "loose": true }],
+    'react-native-reanimated/plugin',
   ],
 };
