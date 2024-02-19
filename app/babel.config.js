e020f6ce1d07e11ec8f6ff42997383cd62d8b63f
0xturboblitz <florent.tavernier@gmail.com>
2024-02-19 09:08:05 -0700

    fix iOS build crash - remove react-native-reanimated - update tamagui to latest version

--- app/babel.config.js
+++ app/babel.config.js
@@ -2,6 +2,5 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     ["@babel/plugin-transform-private-methods", { "loose": true }],
-    'react-native-reanimated/plugin',
   ],
 };
