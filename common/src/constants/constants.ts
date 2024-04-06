7326c7c350ed6546d253d1f28d1c7560fbbb8078
0xturboblitz <florent.tavernier@gmail.com>
2024-04-06 13:57:51 -0700

    new cloudfront urls

--- common/src/constants/constants.ts
+++ common/src/constants/constants.ts
@@ -1,7 +1,7 @@
 export const AWS_ENDPOINT = "https://0pw5u65m3a.execute-api.eu-north-1.amazonaws.com/api-stage/mint"
 
-export const ARKZKEY_URL = "https://d3kogqxemg7tfk.cloudfront.net/proof_of_passport_final_merkle_and_age.arkzkey"
-export const ZKEY_URL = "https://d3kogqxemg7tfk.cloudfront.net/proof_of_passport_final_merkle_and_age.zkey"
+export const ARKZKEY_URL = "https://d8o9bercqupgk.cloudfront.net/proof_of_passport_final_v01.arkzkey"
+export const ZKEY_URL = "https://d8o9bercqupgk.cloudfront.net/proof_of_passport_final_v01.zkey"
 
 export const TREE_DEPTH = 16
 
