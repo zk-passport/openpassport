1deaf1fb83a58ae07dd20ffb2a7860b38758686a
turnoffthiscomputer <colin.remi07@gmail.com>
2024-05-10 07:23:18 +0200

    update circuits inputs, clean

--- circuits/test/register.test.ts
+++ circuits/test/register.test.ts
@@ -5,59 +5,30 @@ const wasm_tester = require("circom_tester").wasm;
 import { buildPoseidon } from 'circomlibjs';
 import { formatMrz } from '../../common/src/utils/utils';
 import { MAX_DATAHASHES_LEN, SignatureAlgorithm, TREE_DEPTH } from "../../common/src/constants/constants";
-import { poseidon2 } from "poseidon-lite";
+import { poseidon4 } from "poseidon-lite";
 import { IMT } from "@zk-kit/imt";
 import { mockPassportData_sha256WithRSAEncryption_65537 } from "../../common/src/utils/mockPassportData";
 import { generateCircuitInputs_Register } from '../../common/src/utils/generateInputs';
+import { packBytes } from "../../common/src/utils/utils";
 
-describe("start testing register.circom", function () {
+describe("Proof of Passport - Circuits - Register flow", function () {
     this.timeout(0);
     let inputs: any;
     let circuit: any;
     let w: any;
-    let mrz: any;
-    let passportData: any;
     let poseidon: any;
+    let commitment: any;
 
     before(async () => {
-
         circuit = await wasm_tester(path.join(__dirname, "../circuits/register_sha256WithRSAEncryption65537.circom"),
             { include: ["node_modules"] },
         );
         poseidon = await buildPoseidon();
         const passportData = mockPassportData_sha256WithRSAEncryption_65537
-
-        const reveal_bitmap = Array(90).fill('1');
-        const address = "0x70997970c51812dc3a010c7d01b50e0d17dc79c8";
-
-        // const generated_inputs = generateCircuitInputs(
-        //     passportData,
-        //     reveal_bitmap,
-        //     address,
-        //     18,
-        //     { developmentMode: true }
-        // );
         inputs = generateCircuitInputs_Register(
             passportData,
             { developmentMode: true }
         );
-
-        // inputs = {
-        //     secret: BigInt(112111112).toString(),
-        //     mrz: generated_inputs.mrz,
-        //     econtent: generated_inputs.dataHashes,
-        //     datahashes_padded_length: generated_inputs.dataHashes.length,
-        //     signed_attributes: generated_inputs.eContentBytes,
-        //     pubkey: generated_inputs.pubkey,
-        //     merkle_root: generated_inputs.root,
-        //     path: generated_inputs.pathIndices,
-        //     siblings: generated_inputs.siblings,
-        //     signature_algorithm: generated_inputs.signatureAlgorithm,
-        //     signature: generated_inputs.signature,
-        // }
-
-        // console.log(JSON.stringify(inputs, null, 2));
-        // w = await circuit.calculateWitness(inputs);
     });
 
     it("compile and load the circuit", async function () {
@@ -65,10 +36,15 @@ describe("start testing register.circom", function () {
     });
     it("calculate witness", async function () {
         w = await circuit.calculateWitness(inputs);
-        let commitment = await circuit.getOutput(w, ["commitment"]);
-        let nullifier = await circuit.getOutput(w, ["nullifier"]);
-        console.log("commitment", commitment);
-        console.log("nullifier", nullifier);
+        let commitment_circom = await circuit.getOutput(w, ["commitment"]);
+        commitment_circom = commitment_circom.commitment;
+        const formattedMrz = formatMrz(inputs.mrz);
+        const mrz_bytes = packBytes(formattedMrz);
+        const commitment_bytes = poseidon4([BigInt(inputs.secret), BigInt(mrz_bytes[0]), BigInt(mrz_bytes[1]), BigInt(mrz_bytes[2])]);
+        const commitment_js = BigInt(poseidon.F.toString(commitment_bytes)).toString();
+        console.log('commitment_js', commitment_js)
+        console.log('commitment_circom', commitment_circom)
+        expect(commitment_circom).to.be.equal(commitment_js);
     });
     it("try to calculate witness with bad inputs", async function () {
         try {
