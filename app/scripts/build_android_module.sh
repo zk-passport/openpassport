7b31656e815148e5d2235aaebe1f55b697a07231
0xturboblitz <florent.tavernier@gmail.com>
2024-05-11 22:35:01 +0900

    bunch of cleaning 🧹

--- app/scripts/build_android_module.sh
+++ app/scripts/build_android_module.sh
@@ -1,76 +1,31 @@
 #!/bin/bash
 
-# This is adapted from mopro
+# Make that not happen if build ios is already done
 
-DEVICE_TYPE="arm64"
-BUILD_MODE="release"
+# cp ../circuits/build/proof_of_passport_cpp/proof_of_passport.cpp witnesscalc/src
+# cp ../circuits/build/proof_of_passport_cpp/proof_of_passport.dat witnesscalc/src
 
-# Determine the architecture and folder based on device type
-case $DEVICE_TYPE in
-    "x86_64")
-        ARCHITECTURE="x86_64-linux-android"
-        FOLDER="x86_64"
-        ;;
-    "x86")
-        ARCHITECTURE="i686-linux-android"
-        FOLDER="x86"
-        ;;
-    "arm")
-        ARCHITECTURE="armv7-linux-androideabi"
-        FOLDER="armeabi-v7a"
-        ;;
-    "arm64")
-        ARCHITECTURE="aarch64-linux-android"
-        FOLDER="arm64-v8a"
-        ;;
-    *)
-        echo -e "${RED}Error: Invalid device type specified in config: $DEVICE_TYPE${DEFAULT}"
-        exit 1
-        ;;
-esac
+# cd witnesscalc/src
+# # This adds the namespace to the circuit file as described in the README
+# last_include=$(grep -n '#include' proof_of_passport.cpp | tail -1 | cut -d: -f1)
+# if [[ "$OSTYPE" == "darwin"* ]]; then
+#   # macOS requires an empty string with the -i flag and handles backslashes differently
+#   sed -i "" "${last_include}a\\
+# namespace CIRCUIT_NAME {" proof_of_passport.cpp
+# else
+#   # Linux
+#   sed -i "${last_include}a \\nnamespace CIRCUIT_NAME {" proof_of_passport.cpp
+# fi
+# echo "}" >> proof_of_passport.cpp
 
-# Determine the library directory and build command based on build mode
-case $BUILD_MODE in
-    "debug")
-        LIB_DIR="debug"
-        COMMAND=""
-        ;;
-    "release")
-        LIB_DIR="release"
-        COMMAND="--release"
-        ;;
-    *)
-        echo -e "${RED}Error: Invalid build mode specified in config: $BUILD_MODE${DEFAULT}"
-        exit 1
-        ;;
-esac
+# cd ../../..
+# git submodule init
+# git submodule update
 
-PROJECT_DIR=$(pwd)
+# cd app/witnesscalc
+cd witnesscalc
+./build_gmp.sh android
+make android
+cd ..
 
-cd ${PROJECT_DIR}/mopro-ffi
-
-echo "[android] Install cargo-ndk"
-cargo install cargo-ndk
-
-# Print appropriate message based on device type
-echo "Using $ARCHITECTURE libmopro_ffi.a ($LIB_DIR) static library..."
-echo "This only works on $FOLDER devices!"
-
-echo "[android] Build target in $BUILD_MODE mode"
-cargo ndk -t ${ARCHITECTURE} build --lib ${COMMAND} 
-
-echo "[android] Copy files in mopro-android/Example/jniLibs/"
-for binary in ${PROJECT_DIR}/mopro-ffi/target/*/*/libmopro_ffi.so; do file $binary; done
-
-mkdir -p jniLibs/${FOLDER}/ && \
-cp ${PROJECT_DIR}/mopro-ffi/target/${ARCHITECTURE}/${LIB_DIR}/libmopro_ffi.so jniLibs/${FOLDER}/libuniffi_mopro.so
-
-echo "[android] Generating Kotlin bindings in $BUILD_MODE mode..."
-cargo run --features=uniffi/cli ${COMMAND} \
---bin uniffi-bindgen \
-generate src/mopro.udl \
---language kotlin
-
-echo "[android] Copy Kotlin bindings to android app"
-cp -r ${PROJECT_DIR}/mopro-ffi/jniLibs/ ${PROJECT_DIR}/android/app/src/main/jniLibs/
-cp -r ${PROJECT_DIR}/mopro-ffi/src/uniffi/ ${PROJECT_DIR}/android/app/src/main/java/uniffi/
+cp ../circuits/build/proof_of_passport_cpp/proof_of_passport.dat android/app/src/main/res/raw/proof_of_passport_dat.dat
