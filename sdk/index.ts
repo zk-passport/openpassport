e933ce5f17888dac5b48247de8f8a7753e56dd0e
turnoffthiscomputer <colin.remi07@gmail.com>
2024-07-08 00:03:41 +0200

    update sdk

--- sdk/index.ts
+++ sdk/index.ts
@@ -1,15 +1,15 @@
-import { ProofOfPassportWeb2Verifier } from './ProofOfPassportWeb2Verifier';
-import { ProofOfPassportWeb2Inputs } from './ProofOfPassportWeb2Verifier';
-import { ProofOfPassportWeb3Verifier } from './ProofOfPassportWeb3Verifier';
+//import { ProofOfPassportWeb2Verifier } from './ProofOfPassportWeb2Verifier';
+//import { ProofOfPassportWeb2Inputs } from './ProofOfPassportWeb2Verifier';
+//import { ProofOfPassportWeb3Verifier } from './ProofOfPassportWeb3Verifier';
 import { verifyProofs } from './ProofOfPassportRegister';
 import { Proof } from './ProofOfPassportRegister';
 import { check_merkle_root } from './ProofOfPassportRegister';
 import { getNullifier, getDSCModulus } from './ProofOfPassportRegister';
 
 export {
-    ProofOfPassportWeb2Verifier,
-    ProofOfPassportWeb3Verifier,
-    ProofOfPassportWeb2Inputs,
+    //ProofOfPassportWeb2Verifier,
+    //ProofOfPassportWeb3Verifier,
+    //ProofOfPassportWeb2Inputs,
     verifyProofs,
     Proof,
     check_merkle_root,
