2ebcd4d3c40a98cde89ec12651f983019e8bca2b
turnoffthiscomputer <colin.remi07@gmail.com>
2024-07-28 10:18:39 +0200

    update sdk

--- sdk/index.ts
+++ sdk/index.ts
@@ -1,13 +1,13 @@
 import { ProofOfPassportWeb2Verifier } from './ProofOfPassportWeb2Verifier';
 import { ProofOfPassportWeb2Inputs } from './ProofOfPassportWeb2Verifier';
-import { ProofOfPassportWeb3Verifier } from './ProofOfPassportWeb3Verifier';
+// import { ProofOfPassportWeb3Verifier } from './ProofOfPassportWeb3Verifier';
 import { QRCodeGenerator } from './QRCodeGenerator';
 import { AppType } from '../common/src/utils/appType';
 
 export {
-    //ProofOfPassportWeb2Verifier,
-    //ProofOfPassportWeb3Verifier,
-    //ProofOfPassportWeb2Inputs,
+    ProofOfPassportWeb2Verifier,
+    // ProofOfPassportWeb3Verifier,
+    ProofOfPassportWeb2Inputs,
     QRCodeGenerator,
     AppType
 }
