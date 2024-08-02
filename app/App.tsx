818ccf4ec3e197370fcab641f5c76c7ded38287b
Juan Diego Carballo <juandiegocarballo2701@gmail.com>
2024-08-02 01:59:10 -0600

    feat: add ttf costum fonts to app

--- app/App.tsx
+++ app/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import "react-native-get-random-values"
-import "@ethersproject/shims"
+import "react-native-get-random-values";
+import "@ethersproject/shims";
 import MainScreen from './src/screens/MainScreen';
 import { Buffer } from 'buffer';
 import { YStack } from 'tamagui';
@@ -9,12 +9,33 @@ import useNavigationStore from './src/stores/navigationStore';
 import { AMPLITUDE_KEY } from '@env';
 import * as amplitude from '@amplitude/analytics-react-native';
 import useUserStore from './src/stores/userStore';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
 global.Buffer = Buffer;
 
+const customFonts = {
+  'Lato-Bold': require('./assets/fonts/Lato-Bold.ttf'),
+  'Lato-BoldItalic': require('./assets/fonts/Lato-BoldItalic.ttf'),
+  'Lato-Italic': require('./assets/fonts/Lato-Italic.ttf'),
+  'Lato-Regular': require('./assets/fonts/Lato-Regular.ttf'),
+  'Luciole-Bold-Italic': require('./assets/fonts/Luciole-Bold-Italic.ttf'),
+  'Luciole-Bold': require('./assets/fonts/Luciole-Bold.ttf'),
+  'Luciole-Regular-Italic': require('./assets/fonts/Luciole-Regular-Italic.ttf'),
+  'Luciole-Regular': require('./assets/fonts/Luciole-Regular.ttf'),
+};
+
 function App(): JSX.Element {
+  const [fontsLoaded, setFontsLoaded] = React.useState(false);
+
   const toast = useToastController();
   const setToast = useNavigationStore((state) => state.setToast);
   const initUserStore = useUserStore((state) => state.initUserStore);
+
+  const loadFonts = async () => {
+    await Font.loadAsync(customFonts);
+    setFontsLoaded(true);
+  };
 
   useEffect(() => {
     setToast(toast);
@@ -27,7 +48,13 @@ function App(): JSX.Element {
     initUserStore();
   }, []);
 
-  // TODO: when passportData already stored, retrieve and jump to main screen
+  useEffect(() => {
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
 
   return (
     <YStack f={1} bc="#161616" h="100%" w="100%">
