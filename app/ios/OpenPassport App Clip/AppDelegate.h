3cf1c41afd4291af917a241c2b1edb7903895ed4
0xturboblitz <florent.tavernier@gmail.com>
2024-08-18 14:31:15 -0700

    config

--- app/ios/OpenPassport App Clip/AppDelegate.h
+++ app/ios/OpenPassport App Clip/AppDelegate.h
@@ -5,10 +5,13 @@
 //  Created by turboblitz on 17/08/2024.
 //
 
+#import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
 
 @property (readonly, strong) NSPersistentContainer *persistentContainer;
 
--- app/ios/OpenPassport App Clip/ViewController.m
+++ app/ios/OpenPassport App Clip/ViewController.m
@@ -6,6 +6,8 @@
 //
 
 #import "ViewController.h"
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 
 @interface ViewController ()
 
@@ -13,9 +15,27 @@
 
 @implementation ViewController
 
+- (void)loadView {
+  #if DEBUG
+  // For DEBUG configuration, javascript will be loaded from index file
+  NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.clip"];
+  #else
+  // For RELEASE configuration, js code is bundled and main.jsbundle file is created
+  NSURL *jsCodeLocation = [[NSBundle mainBundle]
+URLForResource:@"main" withExtension:@"jsbundle"];
+  #endif
+
+  // Value of moduleName should be equal to appName value set in index file
+  RCTRootView *rootView = [[RCTRootView alloc]
+initWithBundleURL:jsCodeLocation moduleName:@"OpenPassport"
+initialProperties:nil launchOptions:nil];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f
+green:1.0f blue:1.0f alpha:1];
+  self.view = rootView;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
 
