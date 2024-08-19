95f1fba784ab3f94d2ad4abaa2dbac0e8390d3bc
turnoffthiscomputer <colin.remi07@gmail.com>
2024-08-19 16:51:17 +0100

    implement prove new circuit

---
+++ circuits/circuits/prove/prove_rsa_65537_sha256.circom
@@ -0,0 +1,63 @@
+pragma circom 2.1.5;
+
+include "circomlib/circuits/poseidon.circom";
+include "@zk-email/circuits/utils/bytes.circom";
+include "../verifier/passport_verifier_rsa_65537_sha256.circom";
+include "binary-merkle-root.circom";
+include "../utils/splitSignalsToWords.circom";
+include "../utils/leafHasher.circom";
+include "../disclose/disclose.circom";
+
+
+template PROVE_RSA_65537_SHA256(n, k, max_datahashes_bytes, signatureAlgorithm) {
+
+    /*** CUSTOM IMPLEMENTATION ***/
+    signal input mrz[93];
+    signal input dg1_hash_offset;
+    signal input dataHashes[max_datahashes_bytes];
+    signal input datahashes_padded_length;
+    signal input eContent[104];
+    signal input signature[k];
+    signal input dsc_modulus[k];
+
+    // generate nullifier
+    component splitSignalsToWords_signature = SplitSignalsToWords(n,k,230,9);
+    splitSignalsToWords_signature.in <== signature;
+    component nullifier_hasher = Poseidon(9);
+    for (var i= 0; i < 9; i++) {
+        nullifier_hasher.inputs[i] <== splitSignalsToWords_signature.out[i];
+
+    }
+
+    // Verify passport validity
+    component PV = PASSPORT_VERIFIER_RSA_65537_SHA256(n, k, max_datahashes_bytes);
+    PV.mrz <== mrz;
+    PV.dg1_hash_offset <== dg1_hash_offset;
+    PV.dataHashes <== dataHashes;
+    PV.datahashes_padded_length <== datahashes_padded_length;
+    PV.eContentBytes <== eContent;
+    PV.dsc_modulus <== dsc_modulus;
+    PV.signature <== signature;
+
+    /*** COMMON TO ALL CIRCUITS ***/
+    signal input scope;
+    signal input bitmap[90];
+    signal input current_date[6]; // YYMMDD - num
+    signal input majority[2]; // YY - ASCII
+    signal input user_identifier; 
+
+    // verify passport validity and disclose optional data
+    component disclose = DISCLOSE();
+    disclose.mrz <== mrz;
+    disclose.bitmap <== bitmap;
+    disclose.current_date <== current_date;
+    disclose.majority <== majority;
+    disclose.user_identifier <== user_identifier;
+    disclose.scope <== scope;
+    disclose.secret <== nullifier_hasher.out;
+    signal output revealedData_packed[3] <== disclose.revealedData_packed;
+    signal output nullifier <== disclose.nullifier;
+
+}
+
+component main { public [dsc_modulus, user_identifier, current_date ]  } = PROVE_RSA_65537_SHA256(121, 17, 320, 1);
--- common/src/utils/generateInputs.ts
+++ common/src/utils/generateInputs.ts
@@ -132,9 +132,9 @@ export function generateCircuitInputsRegister(
     secret: [secret],
     mrz: formattedMrz.map((byte) => String(byte)),
     dg1_hash_offset: [dg1HashOffset.toString()], // uncomment when adding new circuits
-    econtent: Array.from(messagePadded).map((x) => x.toString()),
+    dataHashes: Array.from(messagePadded).map((x) => x.toString()),
     datahashes_padded_length: [messagePaddedLen.toString()],
-    signed_attributes: eContent.map(toUnsignedByte).map((byte) => String(byte)),
+    eContent: eContent.map(toUnsignedByte).map((byte) => String(byte)),
     signature: signature,
     dsc_modulus: dsc_modulus,
     attestation_id: [attestation_id],
@@ -209,24 +209,24 @@ export function generateCircuitInputsOfac(
   scope: string,
   user_identifier: string,
   sparsemerkletree: SMT,
-  proofLevel : number,
+  proofLevel: number,
 ) {
 
-  const result = generateCircuitInputsDisclose(secret,attestation_id,passportData,merkletree,majority,bitmap,scope,user_identifier);
+  const result = generateCircuitInputsDisclose(secret, attestation_id, passportData, merkletree, majority, bitmap, scope, user_identifier);
   const { majority: _, scope: __, bitmap: ___, user_identifier: ____, ...finalResult } = result;
 
   const mrz_bytes = formatMrz(passportData.mrz);
-  const passport_leaf = getPassportNumberLeaf(mrz_bytes.slice(49,58))
-  const namedob_leaf = getNameDobLeaf(mrz_bytes.slice(10,49), mrz_bytes.slice(62, 68)) // [57-62] + 5 shift
-  const name_leaf = getNameLeaf(mrz_bytes.slice(10,49)) // [6-44] + 5 shift
-  
-  let root,closestleaf,siblings;  
-  if(proofLevel == 3){
-    ({root, closestleaf, siblings} = generateSMTProof(sparsemerkletree, passport_leaf));
-  } else if(proofLevel == 2){
-    ({root, closestleaf, siblings} = generateSMTProof(sparsemerkletree, namedob_leaf));
-  } else if (proofLevel == 1){
-    ({root, closestleaf, siblings} = generateSMTProof(sparsemerkletree, name_leaf));
+  const passport_leaf = getPassportNumberLeaf(mrz_bytes.slice(49, 58))
+  const namedob_leaf = getNameDobLeaf(mrz_bytes.slice(10, 49), mrz_bytes.slice(62, 68)) // [57-62] + 5 shift
+  const name_leaf = getNameLeaf(mrz_bytes.slice(10, 49)) // [6-44] + 5 shift
+
+  let root, closestleaf, siblings;
+  if (proofLevel == 3) {
+    ({ root, closestleaf, siblings } = generateSMTProof(sparsemerkletree, passport_leaf));
+  } else if (proofLevel == 2) {
+    ({ root, closestleaf, siblings } = generateSMTProof(sparsemerkletree, namedob_leaf));
+  } else if (proofLevel == 1) {
+    ({ root, closestleaf, siblings } = generateSMTProof(sparsemerkletree, name_leaf));
   } else {
     throw new Error("Invalid proof level")
   }
@@ -253,4 +253,34 @@ export function findIndexInTree(tree: LeanIMT, commitment: bigint): number {
     //  console.log(`Index of commitment in the registry: ${index}`);
   }
   return index;
+}
+
+
+export function generateCircuitInputsProve(
+  passportData: PassportData,
+  n_dsc: number,
+  k_dsc: number,
+  scope: string,
+  bitmap: string[],
+  majority: string,
+  user_identifier: string,
+) {
+
+  const register_inputs = generateCircuitInputsRegister('0', '0', '0', passportData, n_dsc, k_dsc);
+  const current_date = getCurrentDateYYMMDD().map(datePart => BigInt(datePart).toString());
+  return {
+    mrz: register_inputs.mrz,
+    dg1_hash_offset: register_inputs.dg1_hash_offset, // uncomment when adding new circuits
+    dataHashes: register_inputs.dataHashes,
+    datahashes_padded_length: register_inputs.datahashes_padded_length,
+    eContent: register_inputs.eContent,
+    signature: register_inputs.signature,
+    dsc_modulus: register_inputs.dsc_modulus,
+    current_date: current_date,
+    bitmap: bitmap,
+    majority: majority.split('').map(char => BigInt(char.charCodeAt(0)).toString()),
+    user_identifier: user_identifier,
+    scope: [BigInt(scope).toString()]
+  };
+
 }
