bacdfabd50f50929abdff43a835c23689be89d8c
0xturboblitz <florent.tavernier@gmail.com>
2024-08-20 13:10:59 -0700

    fix ecdsa test

--- circuits/tests/register/register_ecdsa_sha256.test.ts
+++ circuits/tests/register/register_ecdsa_sha256.test.ts
@@ -108,7 +108,7 @@ describe('Register - SHA256 WITH ECDSA', function () {
     try {
       const invalidInputs = {
         ...inputs,
-        signature_s: wrong_signature_s,
+        signature: [inputs.signature[0], wrong_signature_s],
       };
       await circuit.calculateWitness(invalidInputs);
       expect.fail('Expected an error but none was thrown.');
