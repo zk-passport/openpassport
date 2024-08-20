55508ffec6403be84f6e3c204da178a602f381cb
0xturboblitz <florent.tavernier@gmail.com>
2024-08-19 19:04:19 -0700

    fix imports

---
+++ app/ios/OpenPassport App Clip-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#include "witnesscalc_register_sha256WithRSAEncryption_65537.h"
+#include "witnesscalc_disclose.h"
+#include "groth16_prover.h"
