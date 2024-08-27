65ef044a5c68ce44245fcf4e03f204b479c8a75b
turnoffthiscomputer <colin.remi07@gmail.com>
2024-08-27 14:51:16 +0200

    uncomment buildJson.ts

--- registry/src/buildJson.ts
+++ registry/src/buildJson.ts
@@ -27,24 +27,24 @@ async function main() {
     const skiPemDevJson = await getSkiPemDevJson();
     fs.writeFileSync(ski_pem_dev_path, JSON.stringify(skiPemDevJson, null, 2));
 
-    // console.log('\x1b[32m', 'building SKI modulus JSON', '\x1b[0m');
-    // const skiModulusJson = await getSkiModulusJson(csca_pem_directory_path);
-    // fs.writeFileSync(ski_modulus_path, JSON.stringify(skiModulusJson, null, 2));
+    console.log('\x1b[32m', 'building SKI modulus JSON', '\x1b[0m');
+    const skiModulusJson = await getSkiModulusJson(csca_pem_directory_path);
+    fs.writeFileSync(ski_modulus_path, JSON.stringify(skiModulusJson, null, 2));
 
 
-    // console.log('\x1b[32m', 'building searchable registry JSON', '\x1b[0m');
-    // const searchable_registry_csca = await getRegistryJson(csca_pem_directory_path);
-    // fs.writeFileSync(searchable_registry_csca_path, JSON.stringify(searchable_registry_csca, null, 2));
+    console.log('\x1b[32m', 'building searchable registry JSON', '\x1b[0m');
+    const searchable_registry_csca = await getRegistryJson(csca_pem_directory_path);
+    fs.writeFileSync(searchable_registry_csca_path, JSON.stringify(searchable_registry_csca, null, 2));
 
-    // const searchable_registry_dsc = await getRegistryJson(dsc_pem_directory_path);
-    // fs.writeFileSync(searchable_registry_dsc_path, JSON.stringify(searchable_registry_dsc, null, 2));
+    const searchable_registry_dsc = await getRegistryJson(dsc_pem_directory_path);
+    fs.writeFileSync(searchable_registry_dsc_path, JSON.stringify(searchable_registry_dsc, null, 2));
 
-    // console.log('\x1b[32m', 'building map JSON', '\x1b[0m');
-    // const map_csca = await getMapJson(csca_pem_directory_path);
-    // fs.writeFileSync(map_csca_path, JSON.stringify(map_csca, null, 2));
+    console.log('\x1b[32m', 'building map JSON', '\x1b[0m');
+    const map_csca = await getMapJson(csca_pem_directory_path);
+    fs.writeFileSync(map_csca_path, JSON.stringify(map_csca, null, 2));
 
-    // const map_dsc = await getMapJson(dsc_pem_directory_path);
-    // fs.writeFileSync(map_dsc_path, JSON.stringify(map_dsc, null, 2));
+    const map_dsc = await getMapJson(dsc_pem_directory_path);
+    fs.writeFileSync(map_dsc_path, JSON.stringify(map_dsc, null, 2));
 }
 
 main().catch(console.error);
