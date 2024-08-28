423f68193ac3d30508c1931be18a1de50ce1e403
turnoffthiscomputer <colin.remi07@gmail.com>
2024-08-28 12:19:02 +0200

    rename ressources

--- app/src/screens/NfcScreen.tsx
+++ app/src/screens/NfcScreen.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { YStack, Text, XStack, Button, ScrollView } from 'tamagui';
 import { bgGreen, borderColor, textBlack, textColor1 } from '../utils/colors';
 import { Carousel } from '../components/Carousel';
-import US_PASSPORT from '../images/us-passport.png'
+import US_PASSPORT from '../images/us_passport.png'
 import REMOVE_CASE from '../images/remove_case.png'
 import US_PASSPORT_LASTPAGE from '../images/passport_lastpage_graybg.png'
 import US_PASSPORT_LASTPAGE_IOS from '../images/passport_lastpage_iphone.png'
