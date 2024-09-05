03506d6da387be4b5a3f970f33d8be799e3f836f
0xturboblitz <florent.tavernier@gmail.com>
2024-09-05 21:29:32 +0900

    start getting invocation link

--- app/ios/OpenPassport App Clip/SceneDelegate.h
+++ app/ios/OpenPassport App Clip/SceneDelegate.h
@@ -10,6 +10,6 @@
 @interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
 
 @property (strong, nonatomic) UIWindow * window;
+@property (strong, readwrite) NSString * initialLinkUrl;
 
 @end
-
