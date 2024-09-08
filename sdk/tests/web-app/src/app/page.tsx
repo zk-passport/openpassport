2bd7b743574f6ac91d2ed6c84aff8a55a4374ddf
turnoffthiscomputer <colin.remi07@gmail.com>
2024-09-08 20:22:52 +0200

    add appName input field to sdk web-app

--- sdk/tests/web-app/src/app/page.tsx
+++ sdk/tests/web-app/src/app/page.tsx
@@ -1,10 +1,15 @@
 'use client';
 
 import { OpenPassportQRcode } from "../../../../src/QRcode/OpenPassportQRcode";
+import { TextField } from "@mui/material";
+import { useState } from "react";
+
 export default function Home() {
+  const [appName, setAppName] = useState("🌐 OpenPassport");
   return (
-    <div className="h-screen w-full bg-white flex items-center justify-center">
-      <OpenPassportQRcode requirements={[["older_than", "18"], ["nationality", "France"]]} appName="OpenPassport" scope="test" userId="test" onSuccess={() => { }} />
+    <div className="h-screen w-full bg-white flex flex-col items-center justify-center gap-12">
+      <OpenPassportQRcode requirements={[["older_than", "18"], ["nationality", "France"]]} appName={appName} scope="test" userId="test" onSuccess={() => { }} />
+      <TextField id="outlined-basic" label="App Name" variant="outlined" value={appName} onChange={(e) => setAppName(e.target.value)} />
     </div>
   );
 }
