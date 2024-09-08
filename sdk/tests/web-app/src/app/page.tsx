956d080d22d6fd4b786ec315d5c32f185098c181
turnoffthiscomputer <colin.remi07@gmail.com>
2024-09-08 20:57:38 +0200

    use uuid for userId

--- sdk/tests/web-app/src/app/page.tsx
+++ sdk/tests/web-app/src/app/page.tsx
@@ -3,12 +3,14 @@
 import { OpenPassportQRcode } from "../../../../src/QRcode/OpenPassportQRcode";
 import { TextField } from "@mui/material";
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Home() {
   const [appName, setAppName] = useState("🌐 OpenPassport");
+  const userId = uuidv4();
   return (
     <div className="h-screen w-full bg-white flex flex-col items-center justify-center gap-12">
-      <OpenPassportQRcode requirements={[["older_than", "18"], ["nationality", "France"]]} appName={appName} scope="test" userId="test" onSuccess={() => { }} />
+      <OpenPassportQRcode requirements={[["nationality", "France"], ["older_than", "18"]]} appName={appName} scope="test" userId={userId} onSuccess={() => { }} devMode={true} />
       <TextField id="outlined-basic" label="App Name" variant="outlined" value={appName} onChange={(e) => setAppName(e.target.value)} />
     </div>
   );
