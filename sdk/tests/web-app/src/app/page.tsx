f9259192a47d4f70b3d4d2b4898e968222b1e700
turnoffthiscomputer <colin.remi07@gmail.com>
2024-09-09 20:16:15 +0200

    clean page.tsx from sdk web-app

--- sdk/tests/web-app/src/app/page.tsx
+++ sdk/tests/web-app/src/app/page.tsx
@@ -1,14 +1,5 @@
 'use client';
-
-import { OpenPassportQRcode } from '../../../../src/QRcode/OpenPassportQRcode';
-// import { OpenPassportQRcode } from '../../../../dist/bundle.web.js'
-import { TextField } from '@mui/material';
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import { COMMITMENT_TREE_TRACKER_URL } from '../../../../../common/src/constants/constants';
 export default function Home() {
-  const [appName, setAppName] = useState('🌐 OpenPassport');
-  const userId = uuidv4();
   return (
     <div className="h-screen w-full bg-gray-100 flex flex-row items-center justify-center gap-12">
       <div
