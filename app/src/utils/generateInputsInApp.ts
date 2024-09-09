3d4f12dd535a503e85c587253e2a21c611c3abb2
turnoffthiscomputer <colin.remi07@gmail.com>
2024-09-09 20:28:38 +0200

    use react const instead of function for generateCircuitInputsInApp

--- app/src/utils/generateInputsInApp.ts
+++ app/src/utils/generateInputsInApp.ts
@@ -5,32 +5,30 @@ import { generateCircuitInputsProve, generateCircuitInputsRegister } from '../..
 import { DEFAULT_MAJORITY, k_dsc, n_dsc, PASSPORT_ATTESTATION_ID } from '../../../common/src/constants/constants';
 import { revealBitmapFromAttributes } from '../../../common/src/utils/revealBitmap';
 import useUserStore from '../stores/userStore';
+import { ArgumentsProve } from '../../../common/src/utils/appType'
 
-
-export function generateCircuitInputsInApp(
+export const generateCircuitInputsInApp = (
     passportData: PassportData,
     app: AppType
-) {
+): any => {
     switch (app.circuit) {
-        case 'register':
-            const {
-                secret,
-                dscSecret
-            } = useUserStore()
+        case 'register': {
+            const { secret, dscSecret } = useUserStore.getState();
             return generateCircuitInputsRegister(secret, dscSecret as string, PASSPORT_ATTESTATION_ID, passportData, n_dsc, k_dsc);
-        case 'prove':
-            const disclosureOptions = (app as any).getDisclosureOptions();
+        }
+        case 'prove': {
+            const disclosureOptions = (app.arguments as ArgumentsProve).disclosureOptions || {};
             return generateCircuitInputsProve(
                 passportData,
                 64, 32,
                 app.scope,
                 revealBitmapFromAttributes(disclosureOptions as any),
-                disclosureOptions?.older_than ?? DEFAULT_MAJORITY,
+                disclosureOptions.older_than ?? DEFAULT_MAJORITY,
                 app.userId,
                 app.userIdType
             );
+        }
         default:
             throw new Error('Invalid circuit');
     }
-
 }
