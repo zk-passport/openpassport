f233542b2d3e150e5a1aa9b25751f46de900520d
turnoffthiscomputer <colin.remi07@gmail.com>
2024-09-12 12:48:27 +0200

    refactor test sheet

--- sdk/tests/openPassportVerifier.test.ts
+++ sdk/tests/openPassportVerifier.test.ts
@@ -1,260 +1,59 @@
-import { assert, expect } from 'chai';
+import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { groth16 } from 'snarkjs';
-import {
-  generateCircuitInputsProve,
-  generateCircuitInputsRegister,
-} from '../../common/src/utils/generateInputs';
 import { OpenPassportVerifier, OpenPassportVerifierInputs } from '../src/OpenPassportVerifier';
 import { genMockPassportData } from '../../common/src/utils/genMockPassportData';
 import { OpenPassportVerifierReport } from '../src/OpenPassportVerifierReport';
-import crypto from 'crypto';
-import {
-  n_dsc,
-  k_dsc,
-  DEFAULT_MAJORITY,
-  PASSPORT_ATTESTATION_ID,
-} from '../../common/src/constants/constants';
-describe('\x1b[95mOpenPassport1Step\x1b[0m', function () {
+import { alphaCode, dateOfBirth, dateOfExpiry, getCountryName, majority, scope, TestCase, testCases } from './utils/testCases';
+import { generateCircuitInputsInSdk } from './utils/generateInputsInSdk';
+
+
+const runTest = async (testCase: TestCase) => {
+  const { circuitType, algorithm, wasmPath, zkeyPath } = testCase;
+  const passportData = genMockPassportData(algorithm, alphaCode, dateOfBirth, dateOfExpiry);
+  const inputs = generateCircuitInputsInSdk(passportData, circuitType);
+  const { proof, publicSignals } = await groth16.fullProve(inputs, wasmPath, zkeyPath);
+  const openPassportVerifier = new OpenPassportVerifier({
+    scope: scope,
+    olderThan: majority,
+    nationality: getCountryName(),
+    dev_mode: true,
+    circuit: circuitType,
+  });
+
+  const openPassportVerifierInputs = new OpenPassportVerifierInputs({
+    dscProof: {
+      publicSignals: publicSignals,
+      proof: proof as any,
+    },
+    circuit: circuitType,
+    dsc: passportData.dsc as string,
+  });
+
+  const result = await openPassportVerifier.verify(openPassportVerifierInputs);
+  return { result, openPassportVerifierInputs };
+};
+
+const verifyResult = (
+  result: OpenPassportVerifierReport,
+  openPassportVerifierInputs: OpenPassportVerifierInputs
+) => {
+  if (!result.valid) {
+    console.log(result);
+  }
+  expect(openPassportVerifierInputs.getNullifier()).not.to.be.null;
+  console.log('\x1b[34mnullifier: \x1b[0m', openPassportVerifierInputs.getNullifier());
+  expect(result.valid).to.be.true;
+};
+
+describe('\x1b[35mOpenPassportVerifier\x1b[0m', function () {
   this.timeout(0);
 
-  /// Define common variables
-  const user_identifier = crypto.randomUUID();
-  const scope = '@spaceShips';
-  const bitmap = Array(90).fill('1');
-
-  describe('\x1b[96mregister circuits\x1b[0m', () => {
-    it('OpenPassport1Step - register - rsa sha256', async function () {
-      const path_prove_wasm = '../circuits/build/fromAWS/register_rsa_65537_sha256.wasm';
-      const path_prove_zkey = '../circuits/build/fromAWS/register_rsa_65537_sha256.zkey';
-      const passportData = genMockPassportData('rsa_sha256', 'FRA', '000101', '300101');
-      const inputs = generateCircuitInputsRegister(
-        BigInt(0).toString(),
-        BigInt(0).toString(),
-        PASSPORT_ATTESTATION_ID,
-        passportData,
-        n_dsc,
-        k_dsc
-      );
-      const { proof, publicSignals } = await groth16.fullProve(
-        inputs,
-        path_prove_wasm,
-        path_prove_zkey
-      );
-      const openPassportVerifier = new OpenPassportVerifier({
-        scope: scope,
-        olderThan: '18',
-        nationality: 'France',
-        dev_mode: true,
-        circuit: 'register',
-      });
-      const openPassportVerifierInputs = new OpenPassportVerifierInputs({
-        dscProof: {
-          publicSignals: publicSignals,
-          proof: proof as any,
-        },
-        circuit: 'register',
-        dsc: passportData.dsc as string,
-      });
-      const result = await openPassportVerifier.verify(openPassportVerifierInputs);
-      verifyResult(result, openPassportVerifierInputs);
-    });
-
-    it('OpenPassport1Step - register - rsa sha1', async function () {
-      const path_prove_wasm = '../circuits/build/fromAWS/register_rsa_65537_sha1.wasm';
-      const path_prove_zkey = '../circuits/build/fromAWS/register_rsa_65537_sha1.zkey';
-      const passportData = genMockPassportData('rsa_sha1', 'FRA', '000101', '300101');
-      const inputs = generateCircuitInputsRegister(
-        BigInt(0).toString(),
-        BigInt(0).toString(),
-        PASSPORT_ATTESTATION_ID,
-        passportData,
-        n_dsc,
-        k_dsc
-      );
-      const { proof, publicSignals } = await groth16.fullProve(
-        inputs,
-        path_prove_wasm,
-        path_prove_zkey
-      );
-      const openPassportVerifier = new OpenPassportVerifier({
-        scope: scope,
-        olderThan: '18',
-        nationality: 'France',
-        dev_mode: true,
-        circuit: 'register',
-      });
-      const openPassportVerifierInputs = new OpenPassportVerifierInputs({
-        dscProof: {
-          publicSignals: publicSignals,
-          proof: proof as any,
-        },
-        circuit: 'register',
-        dsc: passportData.dsc as string,
-      });
-      const result = await openPassportVerifier.verify(openPassportVerifierInputs);
-      verifyResult(result, openPassportVerifierInputs);
-    });
-
-    it('OpenPassport1Step - register - rsapss sha256', async function () {
-      const path_prove_wasm = '../circuits/build/fromAWS/register_rsapss_65537_sha256.wasm';
-      const path_prove_zkey = '../circuits/build/fromAWS/register_rsapss_65537_sha256.zkey';
-      const passportData = genMockPassportData('rsapss_sha256', 'FRA', '000101', '300101');
-      const inputs = generateCircuitInputsRegister(
-        BigInt(0).toString(),
-        BigInt(0).toString(),
-        PASSPORT_ATTESTATION_ID,
-        passportData,
-        n_dsc,
-        k_dsc
-      );
-      const { proof, publicSignals } = await groth16.fullProve(
-        inputs,
-        path_prove_wasm,
-        path_prove_zkey
-      );
-      const openPassportVerifier = new OpenPassportVerifier({
-        scope: scope,
-        olderThan: '18',
-        nationality: 'France',
-        dev_mode: true,
-        circuit: 'register',
-      });
-      const openPassportVerifierInputs = new OpenPassportVerifierInputs({
-        dscProof: {
-          publicSignals: publicSignals,
-          proof: proof as any,
-        },
-        circuit: 'register',
-        dsc: passportData.dsc as string,
-      });
-      const result = await openPassportVerifier.verify(openPassportVerifierInputs);
+  testCases.forEach((testCase) => {
+    const { circuitType, algorithm } = testCase;
+    it(`${circuitType} - ${algorithm}`, async function () {
+      const { result, openPassportVerifierInputs } = await runTest(testCase);
       verifyResult(result, openPassportVerifierInputs);
     });
   });
-
-  describe('\x1b[96mprove circuits\x1b[0m', () => {
-    it('OpenPassport1Step - rsa sha256', async function () {
-      const path_prove_wasm = '../circuits/build/fromAWS/prove_rsa_65537_sha256.wasm';
-      const path_prove_zkey = '../circuits/build/fromAWS/prove_rsa_65537_sha256.zkey';
-      const passportData = genMockPassportData('rsa_sha256', 'FRA', '000101', '300101');
-      const inputs = generateCircuitInputsProve(
-        passportData,
-        n_dsc,
-        k_dsc,
-        scope,
-        bitmap,
-        DEFAULT_MAJORITY,
-        user_identifier
-      );
-      const { proof, publicSignals } = await groth16.fullProve(
-        inputs,
-        path_prove_wasm,
-        path_prove_zkey
-      );
-      const openPassportVerifier = new OpenPassportVerifier({
-        scope: scope,
-        olderThan: '18',
-        nationality: 'France',
-        dev_mode: true,
-        circuit: 'prove',
-      });
-      const openPassportVerifierInputs = new OpenPassportVerifierInputs({
-        dscProof: {
-          publicSignals: publicSignals,
-          proof: proof as any,
-        },
-        dsc: passportData.dsc as string,
-        circuit: 'prove',
-      });
-      const result = await openPassportVerifier.verify(openPassportVerifierInputs);
-      verifyResult(result, openPassportVerifierInputs);
-    });
-
-    it('OpenPassport1Step - rsa sha1', async function () {
-      const path_prove_wasm = '../circuits/build/fromAWS/prove_rsa_65537_sha1.wasm';
-      const path_prove_zkey = '../circuits/build/fromAWS/prove_rsa_65537_sha1.zkey';
-      const passportData = genMockPassportData('rsa_sha1', 'FRA', '000101', '300101');
-      const inputs = generateCircuitInputsProve(
-        passportData,
-        n_dsc,
-        k_dsc,
-        scope,
-        bitmap,
-        DEFAULT_MAJORITY,
-        user_identifier
-      );
-      const { proof, publicSignals } = await groth16.fullProve(
-        inputs,
-        path_prove_wasm,
-        path_prove_zkey
-      );
-      const openPassportVerifier = new OpenPassportVerifier({
-        scope: scope,
-        olderThan: '18',
-        nationality: 'France',
-        dev_mode: true,
-        circuit: 'prove',
-      });
-      const openPassportVerifierInputs = new OpenPassportVerifierInputs({
-        dscProof: {
-          publicSignals: publicSignals,
-          proof: proof as any,
-        },
-        dsc: passportData.dsc as string,
-        circuit: 'prove',
-      });
-      const result = await openPassportVerifier.verify(openPassportVerifierInputs);
-      verifyResult(result, openPassportVerifierInputs);
-    });
-
-    it('OpenPassport1Step - rsapss sha256', async function () {
-      const path_prove_wasm = '../circuits/build/fromAWS/prove_rsapss_65537_sha256.wasm';
-      const path_prove_zkey = '../circuits/build/fromAWS/prove_rsapss_65537_sha256.zkey';
-      const passportData = genMockPassportData('rsapss_sha256', 'FRA', '000101', '300101');
-      const inputs = generateCircuitInputsProve(
-        passportData,
-        n_dsc,
-        k_dsc,
-        scope,
-        bitmap,
-        DEFAULT_MAJORITY,
-        user_identifier
-      );
-      const { proof, publicSignals } = await groth16.fullProve(
-        inputs,
-        path_prove_wasm,
-        path_prove_zkey
-      );
-      const openPassportVerifier = new OpenPassportVerifier({
-        scope: scope,
-        olderThan: '18',
-        nationality: 'France',
-        dev_mode: true,
-        circuit: 'prove',
-      });
-      const openPassportVerifierInputs = new OpenPassportVerifierInputs({
-        dscProof: {
-          publicSignals: publicSignals,
-          proof: proof as any,
-        },
-        dsc: passportData.dsc as string,
-        circuit: 'prove',
-      });
-      const result = await openPassportVerifier.verify(openPassportVerifierInputs);
-      verifyResult(result, openPassportVerifierInputs);
-    });
-  });
-
-  const verifyResult = (
-    result: OpenPassportVerifierReport,
-    openPassportVerifierInputs: OpenPassportVerifierInputs
-  ) => {
-    if (!result.valid) {
-      console.log(result);
-    }
-    expect(openPassportVerifierInputs.getNullifier()).not.to.be.null;
-    console.log('\x1b[34mnullifier: \x1b[0m', openPassportVerifierInputs.getNullifier());
-    expect(result.valid).to.be.true;
-  };
 });
---
+++ sdk/tests/utils/testCases.ts
@@ -0,0 +1,63 @@
+import { countryCodes, DEFAULT_MAJORITY } from "../../../common/src/constants/constants";
+
+type CircuitType = 'register' | 'prove';
+type AlgorithmType = 'rsa_sha256' | 'rsa_sha1' | 'rsapss_sha256';
+
+export const scope = '@spaceShips';
+export const majority = DEFAULT_MAJORITY;
+export const alphaCode = 'FRA';
+export const dateOfBirth = '000101';
+export const dateOfExpiry = '300101';
+export const getCountryName = () => {
+  const countryName = countryCodes[alphaCode as keyof typeof countryCodes];
+  if (!countryName) {
+    throw new Error(`Country name not found for alpha code: ${alphaCode}`);
+  }
+  return countryName;
+};
+
+export interface TestCase {
+  circuitType: CircuitType;
+  algorithm: AlgorithmType;
+  wasmPath: string;
+  zkeyPath: string;
+}
+
+export const testCases: TestCase[] = [
+  {
+    circuitType: 'register',
+    algorithm: 'rsa_sha256',
+    wasmPath: '../circuits/build/fromAWS/register_rsa_65537_sha256.wasm',
+    zkeyPath: '../circuits/build/fromAWS/register_rsa_65537_sha256.zkey',
+  },
+  {
+    circuitType: 'register',
+    algorithm: 'rsa_sha1',
+    wasmPath: '../circuits/build/fromAWS/register_rsa_65537_sha1.wasm',
+    zkeyPath: '../circuits/build/fromAWS/register_rsa_65537_sha1.zkey',
+  },
+  {
+    circuitType: 'register',
+    algorithm: 'rsapss_sha256',
+    wasmPath: '../circuits/build/fromAWS/register_rsapss_65537_sha256.wasm',
+    zkeyPath: '../circuits/build/fromAWS/register_rsapss_65537_sha256.zkey',
+  },
+  {
+    circuitType: 'prove',
+    algorithm: 'rsa_sha256',
+    wasmPath: '../circuits/build/fromAWS/prove_rsa_65537_sha256.wasm',
+    zkeyPath: '../circuits/build/fromAWS/prove_rsa_65537_sha256.zkey',
+  },
+  {
+    circuitType: 'prove',
+    algorithm: 'rsa_sha1',
+    wasmPath: '../circuits/build/fromAWS/prove_rsa_65537_sha1.wasm',
+    zkeyPath: '../circuits/build/fromAWS/prove_rsa_65537_sha1.zkey',
+  },
+  {
+    circuitType: 'prove',
+    algorithm: 'rsapss_sha256',
+    wasmPath: '../circuits/build/fromAWS/prove_rsapss_65537_sha256.wasm',
+    zkeyPath: '../circuits/build/fromAWS/prove_rsapss_65537_sha256.zkey',
+  },
+];
