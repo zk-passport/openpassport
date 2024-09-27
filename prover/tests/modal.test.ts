2f7434731c71267ff951ab37b746faa31cf97413
turnoffthiscomputer <colin.remi07@gmail.com>
2024-09-27 16:59:29 +0200

    update prover

--- prover/tests/modal.test.ts
+++ prover/tests/modal.test.ts
@@ -1,4 +1,4 @@
-import { getCSCAInputs, sendCSCARequest } from "../../common/src/utils/csca";
+import { generateCircuitInputsDSC, sendCSCARequest } from "../../common/src/utils/csca";
 import { mock_csca_sha256_rsa_4096, mock_dsc_sha256_rsa_4096, mock_csca_sha1_rsa_4096, mock_dsc_sha1_rsa_4096, mock_csca_sha256_rsapss_4096, mock_dsc_sha256_rsapss_4096 } from "../../common/src/constants/mockCertificates";
 import forge from "node-forge";
 import { k_csca, k_dsc, MODAL_SERVER_ADDRESS, n_csca, n_dsc } from "../../common/src/constants/constants";
@@ -9,32 +9,20 @@ import { expect } from "chai";
 import path from 'path';
 
 const max_cert_bytes = 1664;
-const dscCert_sha256_rsa = forge.pki.certificateFromPem(mock_dsc_sha256_rsa_4096);
-const cscaCert_sha256_rsa = forge.pki.certificateFromPem(mock_csca_sha256_rsa_4096);
-const dscCert_sha1_rsa = forge.pki.certificateFromPem(mock_dsc_sha1_rsa_4096);
-const cscaCert_sha1_rsa = forge.pki.certificateFromPem(mock_csca_sha1_rsa_4096);
-const dscCert_sha256_rsapss = forge.pki.certificateFromPem(mock_dsc_sha256_rsapss_4096);
-const cscaCert_sha256_rsapss = forge.pki.certificateFromPem(mock_csca_sha256_rsapss_4096);
 
-const vkey_sha256_rsa = JSON.parse(fs.readFileSync(path.join(__dirname, '../src/vkey/dsc_sha256_rsa_4096_vkey.json'), 'utf8'));
-const vkey_sha256_rsapss = JSON.parse(fs.readFileSync(path.join(__dirname, '../src/vkey/dsc_sha256_rsapss_4096_vkey.json'), 'utf8'));
-const vkey_sha1_rsa = JSON.parse(fs.readFileSync(path.join(__dirname, '../src/vkey/dsc_sha1_rsa_4096_vkey.json'), 'utf8'));
+const vkey_sha256_rsa = JSON.parse(fs.readFileSync(path.join(__dirname, '../src/vkey/dsc_rsa_65537_sha256_4096_vkey.json'), 'utf8'));
+const vkey_sha256_rsapss = JSON.parse(fs.readFileSync(path.join(__dirname, '../src/vkey/dsc_rsapss_65537_sha256_4096_vkey.json'), 'utf8'));
+const vkey_sha1_rsa = JSON.parse(fs.readFileSync(path.join(__dirname, '../src/vkey/dsc_rsa_65537_sha1_4096_vkey.json'), 'utf8'));
 
 describe('MODAL PROVER', function () {
     this.timeout(0); // Disable timeout
 
     describe("SHA256 RSAPSS", async () => {
         it("verify proof", async () => {
-            const inputs = getCSCAInputs(
+            const inputs = generateCircuitInputsDSC(
                 BigInt(0).toString(),
-                dscCert_sha256_rsapss,
-                cscaCert_sha256_rsapss,
-                n_dsc,
-                k_dsc,
-                n_csca,
-                k_csca,
+                mock_dsc_sha256_rsapss_4096,
                 max_cert_bytes,
-                true
             );
             console.log('\x1b[34msending request to modal server\x1b[0m');
             const response = await sendCSCARequest(inputs);
@@ -46,21 +34,15 @@ describe('MODAL PROVER', function () {
                 proof.proof
             )
             expect(verifyProof).to.be.true;
-            console.log('\x1b[32mproof for sha256 rsa verified\x1b[0m');
+            console.log('\x1b[32mproof for sha256 rsapss verified\x1b[0m');
         });
     });
     describe("SHA256 RSA", async () => {
         it("verify proof", async () => {
-            const inputs = getCSCAInputs(
+            const inputs = generateCircuitInputsDSC(
                 BigInt(0).toString(),
-                dscCert_sha256_rsa,
-                cscaCert_sha256_rsa,
-                n_dsc,
-                k_dsc,
-                n_csca,
-                k_csca,
+                mock_dsc_sha256_rsa_4096,
                 max_cert_bytes,
-                true
             );
             //console.log(JSON.stringify(inputs));
             console.log('\x1b[34msending request to modal server\x1b[0m');
@@ -79,18 +61,11 @@ describe('MODAL PROVER', function () {
 
     describe("SHA1 RSA", async () => {
         it("verify proof", async () => {
-            const inputs = getCSCAInputs(
+            const inputs = generateCircuitInputsDSC(
                 BigInt(0).toString(),
-                dscCert_sha1_rsa,
-                cscaCert_sha1_rsa,
-                n_dsc,
-                k_dsc,
-                n_csca,
-                k_csca,
+                mock_dsc_sha1_rsa_4096,
                 max_cert_bytes,
-                true
             );
-            console.log(JSON.stringify(inputs));
 
             console.log('\x1b[34msending request to modal server\x1b[0m');
             const response = await sendCSCARequest(inputs);
