f50e7eeeb0ce8f80edc11c6cbe4229a81212fc10
motemotech <i.am.nicoshark@gmail.com>
2024-10-08 00:59:40 +0900

    delete attributePositions and made library to get attribute from passport format

--- contracts/contracts/OneTimeSBT.sol
+++ contracts/contracts/OneTimeSBT.sol
@@ -2,41 +2,27 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 import {IVerifiersManager} from "./interfaces/IVerifiersManager.sol";
-import "./constants/constants.sol";
-
 import {Base64} from "./libraries/Base64.sol";
 import {Formatter} from "./Formatter.sol";
-import {IRegister} from "./interfaces/IRegister.sol";
+import "./constants/constants.sol";
+import "./libraries/AttributeLibrary.sol";
 import "hardhat/console.sol";
 
-contract OneTimeSBT is ERC721Enumerable, Ownable {
+contract OneTimeSBT is ERC721Enumerable {
     using Strings for uint256;
     using Base64 for *;
 
     IVerifiersManager public verifiersManager;
     Formatter public formatter;
 
-    uint constant scope = 1;
-
-    mapping(uint256 => bool) public nullifiers;
     mapping(uint256 => uint256) public sbtExpiration;
-
-    struct AttributePosition {
-        string name;
-        uint256 start;
-        uint256 end;
-        uint256 index;
-    }
 
     struct Attributes {
         string[8] values;
     }
-
-    AttributePosition[] public attributePositions;
 
     mapping(uint256 => Attributes) private tokenAttributes;
 
@@ -46,21 +32,6 @@ contract OneTimeSBT is ERC721Enumerable, Ownable {
     ) ERC721("OpenPassport", "OpenPassport") {
         verifiersManager = v;
         formatter = f;
-        setupAttributes();
-        transferOwnership(msg.sender);
-    }
-
-    function setupAttributes() internal {
-        attributePositions.push(AttributePosition("issuing_state", 2, 4, 0));
-        attributePositions.push(AttributePosition("name", 5, 43, 1));
-        attributePositions.push(
-            AttributePosition("passport_number", 44, 52, 2)
-        );
-        attributePositions.push(AttributePosition("nationality", 54, 56, 3));
-        attributePositions.push(AttributePosition("date_of_birth", 57, 62, 4));
-        attributePositions.push(AttributePosition("gender", 64, 64, 5));
-        attributePositions.push(AttributePosition("expiry_date", 65, 70, 6));
-        attributePositions.push(AttributePosition("older_than", 88, 89, 7));
     }
 
     function mint(
@@ -85,25 +56,20 @@ contract OneTimeSBT is ERC721Enumerable, Ownable {
         );
 
         // check blinded dcs
-        if (keccak256(abi.encodePacked(p_proof.pubSignals[PROVE_RSA_BLINDED_DSC_COMMITMENT_INDEX])) != keccak256(abi.encodePacked(d_proof.pubSignals[DSC_BLINDED_DSC_COMMITMENT_INDEX]))) {
+        if (
+            keccak256(abi.encodePacked(p_proof.pubSignals[PROVE_RSA_BLINDED_DSC_COMMITMENT_INDEX])) !=
+            keccak256(abi.encodePacked(d_proof.pubSignals[DSC_BLINDED_DSC_COMMITMENT_INDEX]))
+        ) {
             revert ("Blinded DSC commitments are not equal");
         }
 
-        if (!verifiersManager.verifyWithProveVerifier(
-                prove_verifier_id,
-                p_proof
-        )) {
+        if (!verifiersManager.verifyWithProveVerifier(prove_verifier_id, p_proof)) {
             revert ("Invalid Prove Proof");
         }
 
-        if (!verifiersManager.verifyWithDscVerifier(
-            dsc_verifier_id,
-            d_proof
-        )) {
+        if (!verifiersManager.verifyWithDscVerifier(dsc_verifier_id, d_proof)) {
             revert ("Invalid DSC Proof");
         }
-
-        nullifiers[p_proof.pubSignals[PROVE_RSA_NULLIFIER_INDEX]] = true;
 
         // Effects: Mint token
         address addr = address(uint160(p_proof.pubSignals[PROVE_RSA_USER_IDENTIFIER_INDEX]));
@@ -121,18 +87,14 @@ contract OneTimeSBT is ERC721Enumerable, Ownable {
 
         Attributes storage attributes = tokenAttributes[newTokenId];
 
-        for (uint i = 0; i < attributePositions.length; i++) {
-            AttributePosition memory attribute = attributePositions[i];
-            bytes memory attributeBytes = new bytes(
-                attribute.end - attribute.start + 1
-            );
-            for (uint j = attribute.start; j <= attribute.end; j++) {
-                attributeBytes[j - attribute.start] = charcodes[j];
-            }
-            string memory attributeValue = string(attributeBytes);
-            attributes.values[i] = attributeValue;
-            // console.log(attribute.name, attributes.values[i]);
-        }
+        attributes.values[0] = AttributeLibrary.getIssuingState(charcodes);
+        attributes.values[1] = AttributeLibrary.getName(charcodes);
+        attributes.values[2] = AttributeLibrary.getPassportNumber(charcodes);
+        attributes.values[3] = AttributeLibrary.getNationality(charcodes);
+        attributes.values[4] = AttributeLibrary.getDateOfBirth(charcodes);
+        attributes.values[5] = AttributeLibrary.getGender(charcodes);
+        attributes.values[6] = AttributeLibrary.getExpiryDate(charcodes);
+        attributes.values[7] = AttributeLibrary.getOlderThan(charcodes);
 
         sbtExpiration[newTokenId] = block.timestamp + 90 days;
     }
@@ -192,45 +154,45 @@ contract OneTimeSBT is ERC721Enumerable, Ownable {
     function getIssuingStateOf(
         uint256 _tokenId
     ) public view returns (string memory) {
-        return tokenAttributes[_tokenId].values[0];
+        return tokenAttributes[_tokenId].values[ATTRIBUTE_ISSUING_STATE_INDEX];
     }
 
     function getNameOf(uint256 _tokenId) public view returns (string memory) {
-        return tokenAttributes[_tokenId].values[1];
+        return tokenAttributes[_tokenId].values[ATTRIBUTE_NAME_INDEX];
     }
 
     function getPassportNumberOf(
         uint256 _tokenId
     ) public view returns (string memory) {
-        return tokenAttributes[_tokenId].values[2];
+        return tokenAttributes[_tokenId].values[ATTRIBUTE_PASSPORT_NUMBER_INDEX];
     }
 
     function getNationalityOf(
         uint256 _tokenId
     ) public view returns (string memory) {
-        return tokenAttributes[_tokenId].values[3];
+        return tokenAttributes[_tokenId].values[ATTRIBUTE_NATIONALITY_INDEX];
     }
 
     function getDateOfBirthOf(
         uint256 _tokenId
     ) public view returns (string memory) {
-        return tokenAttributes[_tokenId].values[4];
+        return tokenAttributes[_tokenId].values[ATTRIBUTE_DATE_OF_BIRTH_INDEX];
     }
 
     function getGenderOf(uint256 _tokenId) public view returns (string memory) {
-        return tokenAttributes[_tokenId].values[5];
+        return tokenAttributes[_tokenId].values[ATTRIBUTE_GENDER_INDEX];
     }
 
     function getExpiryDateOf(
         uint256 _tokenId
     ) public view returns (string memory) {
-        return tokenAttributes[_tokenId].values[6];
+        return tokenAttributes[_tokenId].values[ATTRIBUTE_EXPIRY_DATE_INDEX];
     }
 
     function getOlderThanOf(
         uint256 _tokenId
     ) public view returns (string memory) {
-        return tokenAttributes[_tokenId].values[7];
+        return tokenAttributes[_tokenId].values[ATTRIBUTE_OLDER_THAN_INDEX];
     }
 
     // This is the function to check if the sbt is not expired or not
---
+++ contracts/contracts/libraries/AttributeLibrary.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+library AttributeLibrary {
+    // Define constant start and end positions for each attribute
+    uint256 private constant ISSUING_STATE_START = 2;
+    uint256 private constant ISSUING_STATE_END = 4;
+
+    uint256 private constant NAME_START = 5;
+    uint256 private constant NAME_END = 43;
+
+    uint256 private constant PASSPORT_NUMBER_START = 44;
+    uint256 private constant PASSPORT_NUMBER_END = 52;
+
+    uint256 private constant NATIONALITY_START = 54;
+    uint256 private constant NATIONALITY_END = 56;
+
+    uint256 private constant DATE_OF_BIRTH_START = 57;
+    uint256 private constant DATE_OF_BIRTH_END = 62;
+
+    uint256 private constant GENDER_START = 64;
+    uint256 private constant GENDER_END = 64;
+
+    uint256 private constant EXPIRY_DATE_START = 65;
+    uint256 private constant EXPIRY_DATE_END = 70;
+
+    uint256 private constant OLDER_THAN_START = 88;
+    uint256 private constant OLDER_THAN_END = 89;
+
+    /**
+     * @notice Extracts the issuing state from the charcodes.
+     * @param charcodes The bytes array containing packed attribute data.
+     * @return The issuing state as a string.
+     */
+    function getIssuingState(bytes memory charcodes) internal pure returns (string memory) {
+        return extractAttribute(charcodes, ISSUING_STATE_START, ISSUING_STATE_END);
+    }
+
+    /**
+     * @notice Extracts the name from the charcodes.
+     * @param charcodes The bytes array containing packed attribute data.
+     * @return The name as a string.
+     */
+    function getName(bytes memory charcodes) internal pure returns (string memory) {
+        return extractAttribute(charcodes, NAME_START, NAME_END);
+    }
+
+    /**
+     * @notice Extracts the passport number from the charcodes.
+     * @param charcodes The bytes array containing packed attribute data.
+     * @return The passport number as a string.
+     */
+    function getPassportNumber(bytes memory charcodes) internal pure returns (string memory) {
+        return extractAttribute(charcodes, PASSPORT_NUMBER_START, PASSPORT_NUMBER_END);
+    }
+
+    /**
+     * @notice Extracts the nationality from the charcodes.
+     * @param charcodes The bytes array containing packed attribute data.
+     * @return The nationality as a string.
+     */
+    function getNationality(bytes memory charcodes) internal pure returns (string memory) {
+        return extractAttribute(charcodes, NATIONALITY_START, NATIONALITY_END);
+    }
+
+    /**
+     * @notice Extracts the date of birth from the charcodes.
+     * @param charcodes The bytes array containing packed attribute data.
+     * @return The date of birth as a string.
+     */
+    function getDateOfBirth(bytes memory charcodes) internal pure returns (string memory) {
+        return extractAttribute(charcodes, DATE_OF_BIRTH_START, DATE_OF_BIRTH_END);
+    }
+
+    /**
+     * @notice Extracts the gender from the charcodes.
+     * @param charcodes The bytes array containing packed attribute data.
+     * @return The gender as a string.
+     */
+    function getGender(bytes memory charcodes) internal pure returns (string memory) {
+        return extractAttribute(charcodes, GENDER_START, GENDER_END);
+    }
+
+    /**
+     * @notice Extracts the expiry date from the charcodes.
+     * @param charcodes The bytes array containing packed attribute data.
+     * @return The expiry date as a string.
+     */
+    function getExpiryDate(bytes memory charcodes) internal pure returns (string memory) {
+        return extractAttribute(charcodes, EXPIRY_DATE_START, EXPIRY_DATE_END);
+    }
+
+    /**
+     * @notice Extracts the "older than" attribute from the charcodes.
+     * @param charcodes The bytes array containing packed attribute data.
+     * @return The "older than" value as a string.
+     */
+    function getOlderThan(bytes memory charcodes) internal pure returns (string memory) {
+        return extractAttribute(charcodes, OLDER_THAN_START, OLDER_THAN_END);
+    }
+
+    /**
+     * @notice Extracts a substring from the charcodes based on start and end indices.
+     * @param charcodes The bytes array containing packed attribute data.
+     * @param start The starting index (inclusive).
+     * @param end The ending index (inclusive).
+     * @return The extracted substring as a string.
+     */
+    function extractAttribute(bytes memory charcodes, uint256 start, uint256 end) internal pure returns (string memory) {
+        require(charcodes.length > end, "AttributeLibrary: charcodes length insufficient");
+        bytes memory attributeBytes = new bytes(end - start + 1);
+        for (uint256 i = start; i <= end; i++) {
+            attributeBytes[i - start] = charcodes[i];
+        }
+        return string(attributeBytes);
+    }
+}
