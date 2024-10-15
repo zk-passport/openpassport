73b54670b1832f3b2d5f3f1aa4b7326d551eccf6
turnoffthiscomputer <colin.remi07@gmail.com>
2024-10-15 14:15:46 -0700

    implement fallback to register on proofofpassport-merkle-tree.xyz

--- sdk/tests/web-app/src/app/register/page.tsx
+++ sdk/tests/web-app/src/app/register/page.tsx
@@ -3,11 +3,13 @@
 import { OpenPassportQRcode } from '../../../../../src/QRcode/OpenPassportQRcode';
 import { v4 as uuidv4 } from 'uuid';
 import { OpenPassportVerifier } from '../../../../../src/OpenPassportVerifier';
+import { COMMITMENT_TREE_TRACKER_URL } from '../../../../../../common/src/constants/constants';
+import axios from 'axios';
 export default function Prove() {
   const userId = uuidv4();
   const scope = 'scope';
 
-  const openPassportVerifier = new OpenPassportVerifier('register', scope);
+  const openPassportVerifier = new OpenPassportVerifier('register', scope).setCommitmentMerkleTreeUrl(COMMITMENT_TREE_TRACKER_URL);
   return (
     <div className="h-screen w-full bg-white flex flex-col items-center justify-center gap-4">
       <OpenPassportQRcode
@@ -16,7 +18,11 @@ export default function Prove() {
         userIdType={'uuid'}
         openPassportVerifier={openPassportVerifier}
         onSuccess={(attestation) => {
-          // send the code to the backend server
+          axios.post('https://proofofpassport-merkle-tree.xyz/api/verifier/register', attestation).then((response) => {
+            console.log('Registration response:', response);
+          }).catch((error) => {
+            console.error('Error registering attestation:', error);
+          });
         }}
       />
     </div>
