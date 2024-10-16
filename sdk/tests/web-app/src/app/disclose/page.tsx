2d9aed5051a8b3e0767d693851d685bdf9eba29c
turnoffthiscomputer <colin.remi07@gmail.com>
2024-10-16 11:42:34 -0700

    separate regsiter and disclose on 2 different pages

--- sdk/tests/web-app/src/app/disclose/page.tsx
+++ sdk/tests/web-app/src/app/disclose/page.tsx
@@ -4,24 +4,19 @@ import { OpenPassportQRcode } from '../../../../../src/QRcode/OpenPassportQRcode
 import { v4 as uuidv4 } from 'uuid';
 import { OpenPassportVerifier } from '../../../../../src/OpenPassportVerifier';
 import { COMMITMENT_TREE_TRACKER_URL } from '../../../../../../common/src/constants/constants';
-import axios from 'axios';
 export default function Prove() {
   const userId = uuidv4();
   const scope = 'scope';
-  const openPassportVerifier = new OpenPassportVerifier('register', scope).setCommitmentMerkleTreeUrl(COMMITMENT_TREE_TRACKER_URL);
+
+  const openPassportVerifierDisclose = new OpenPassportVerifier('vc_and_disclose', scope).setCommitmentMerkleTreeUrl(COMMITMENT_TREE_TRACKER_URL).excludeCountries('Albania').setMinimumAge(20).enableOFACCheck().setNationality('Germany');
   return (
     <div className="h-screen w-full bg-white flex flex-col items-center justify-center gap-4">
       <OpenPassportQRcode
         appName="Mock App"
         userId={userId}
         userIdType={'uuid'}
-        openPassportVerifier={openPassportVerifier}
+        openPassportVerifier={openPassportVerifierDisclose}
         onSuccess={(attestation) => {
-          axios.post('https://proofofpassport-merkle-tree.xyz/api/verifier/register', attestation).then((response) => {
-            console.log('Registration response:', response);
-          }).catch((error) => {
-            console.error('Error registering attestation:', error);
-          });
         }}
       />
     </div>
