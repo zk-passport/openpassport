fccffa7a54eb073f5ed66645dde41b267c7e9dfc
turnoffthiscomputer <colin.remi07@gmail.com>
2025-01-03 14:35:13 +0100

    update buildJson.ts

--- registry/src/buildJson.ts
+++ registry/src/buildJson.ts
@@ -1,50 +1,57 @@
 import fs from 'fs';
 import path from 'path';
-import { getRegistryJson } from './utils/getRegistryJson';
-import { getMapJson } from './utils/getMapJson';
-import { getSkiModulusJson } from './utils/getSkiModulusJson';
-import { getSkiPemDevJson, getSkiPemJson } from './utils/getSkiPem';
+import { parseCertificateSimple } from '../../common/src/utils/certificate_parsing/parseCertificateSimple';
 
-const csca_pem_directory_path = path.join(__dirname, '..', 'outputs', 'unique_pem_masterlist');
-const dsc_pem_directory_path = path.join(__dirname, '..', 'outputs', 'dsc', 'pem_masterlist');
-
-const searchable_registry_csca_path = path.join(__dirname, '..', 'outputs', 'searchable_registry_csca.json');
-const searchable_registry_dsc_path = path.join(__dirname, '..', 'outputs', 'searchable_registry_dsc.json');
-
-const map_csca_path = path.join(__dirname, '..', 'outputs', 'map_csca.json');
-const map_dsc_path = path.join(__dirname, '..', 'outputs', 'map_dsc.json');
-const ski_modulus_path = path.join(__dirname, '..', 'outputs', 'ski_modulus.json');
+const pemDirectory = path.join(__dirname, '..', 'outputs', 'csca', 'pem_masterlist');
 const ski_pem_path = path.join(__dirname, '..', 'outputs', 'ski_pem.json');
 const ski_pem_dev_path = path.join(__dirname, '..', 'outputs', 'ski_pem_dev.json');
+const dev_pem_path = path.join(__dirname, '..', '..', 'common', 'src', 'mock_certificates');
+
+async function getAllDevPem() {
+    // for all subdirectory in dev_pem_path, check inside each directory for 'mock_csca.pem' or 'mock_csca.cer'
+    let certificates: string[] = [];
+    const subdirectories = fs.readdirSync(dev_pem_path, { withFileTypes: true })
+        .filter(item => item.isDirectory())
+        .map(item => item.name);
+    for (const subdirectory of subdirectories) {
+        const pemFilePath = path.join(dev_pem_path, subdirectory, 'mock_csca.pem');
+        if (fs.existsSync(pemFilePath)) {
+            const pemFile = fs.readFileSync(pemFilePath, 'utf8');
+            certificates.push(pemFile);
+        }
+        const cerFilePath = path.join(dev_pem_path, subdirectory, 'mock_csca.cer');
+        if (fs.existsSync(cerFilePath)) {
+            const cerFile = fs.readFileSync(cerFilePath, 'utf8');
+            certificates.push(cerFile);
+        }
+    }
+    return certificates;
+}
 
 async function main() {
+    const skiPemDevJson: {
+        [key: string]: string
+    } = {};
+    const skiPemJson: {
+        [key: string]: string
+    } = {};
 
-    console.log('\x1b[32m', 'building SKI Pem JSON', '\x1b[0m');
-    const skiPemJson = await getSkiPemJson(csca_pem_directory_path);
-    fs.writeFileSync(ski_pem_path, JSON.stringify(skiPemJson, null, 2));
+    const devCertificates = await getAllDevPem();
 
-    console.log('\x1b[32m', 'building SKI Pem Dev JSON', '\x1b[0m');
-    const skiPemDevJson = await getSkiPemDevJson();
-    fs.writeFileSync(ski_pem_dev_path, JSON.stringify(skiPemDevJson, null, 2));
+    const prodCertificates = fs.readdirSync(pemDirectory);
 
-    console.log('\x1b[32m', 'building SKI modulus JSON', '\x1b[0m');
-    const skiModulusJson = await getSkiModulusJson(csca_pem_directory_path);
-    fs.writeFileSync(ski_modulus_path, JSON.stringify(skiModulusJson, null, 2));
+    for (const prodCertificate of prodCertificates) {
+        const pemContent = fs.readFileSync(path.join(pemDirectory, prodCertificate), 'utf8');
+        try {
+            const certificateData = parseCertificateSimple(pemContent);
+            skiPemJson[certificateData.subjectKeyIdentifier] = pemContent;
+            skiPemDevJson[certificateData.subjectKeyIdentifier] = pemContent;
+        } catch (error) {
+            console.log('\x1b[90m%s\x1b[0m', `certificate ${prodCertificate} is invalid.`);
+        }
+    }
 
-
-    console.log('\x1b[32m', 'building searchable registry JSON', '\x1b[0m');
-    const searchable_registry_csca = await getRegistryJson(csca_pem_directory_path);
-    fs.writeFileSync(searchable_registry_csca_path, JSON.stringify(searchable_registry_csca, null, 2));
-
-    const searchable_registry_dsc = await getRegistryJson(dsc_pem_directory_path);
-    fs.writeFileSync(searchable_registry_dsc_path, JSON.stringify(searchable_registry_dsc, null, 2));
-
-    console.log('\x1b[32m', 'building map JSON', '\x1b[0m');
-    const map_csca = await getMapJson(csca_pem_directory_path);
-    fs.writeFileSync(map_csca_path, JSON.stringify(map_csca, null, 2));
-
-    const map_dsc = await getMapJson(dsc_pem_directory_path);
-    fs.writeFileSync(map_dsc_path, JSON.stringify(map_dsc, null, 2));
+    console.log(skiPemJson);
 }
 
 main().catch(console.error);
