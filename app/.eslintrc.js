bd09683f9477877e7e3e7c82ae5c09e482e777d3
thomas-senechal <thomas.senechal@pm.me>
2025-01-05 15:44:44 +0100

    Add ESLint and Prettier ignore patterns for specific directories

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  ignorePatterns: [
+    'ios/',
+    'android/',
+    'deployments/',
+    'witnesscalc/',
+    'node_modules/',
+  ],
 };
