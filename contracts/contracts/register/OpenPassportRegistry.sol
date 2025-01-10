2258cb2cd11cd03c07efdd617c559a5ad5c5de4e
motemotech <i.am.nicoshark@gmail.com>
2025-01-11 04:00:31 +0900

    remove commentout in contract

--- contracts/contracts/register/OpenPassportRegistry.sol
+++ contracts/contracts/register/OpenPassportRegistry.sol
@@ -1,26 +1,26 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.18;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
-// import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-// contract OpenPassportRegistry is Ownable {
-//     bytes32 public merkleRoot;
+contract OpenPassportRegistry is Ownable {
+    bytes32 public merkleRoot;
 
-//     constructor(bytes32 _merkleRoot) Ownable(msg.sender) {
-//         merkleRoot = _merkleRoot;
-//         transferOwnership(msg.sender);
-//     }
+    constructor(bytes32 _merkleRoot) Ownable(msg.sender) {
+        merkleRoot = _merkleRoot;
+        transferOwnership(msg.sender);
+    }
 
-//     function update(bytes32 _merkleRoot) public onlyOwner {
-//         merkleRoot = _merkleRoot;
-//     }
+    function update(bytes32 _merkleRoot) public onlyOwner {
+        merkleRoot = _merkleRoot;
+    }
 
-//     function checkRoot(bytes32 _merkleRoot) public view returns (bool) {
-//         return merkleRoot == _merkleRoot;
-//     }
+    function checkRoot(bytes32 _merkleRoot) public view returns (bool) {
+        return merkleRoot == _merkleRoot;
+    }
 
-//     function getMerkleRoot() public view returns (bytes32) {
-//         return merkleRoot;
-//     }
-// }
+    function getMerkleRoot() public view returns (bytes32) {
+        return merkleRoot;
+    }
+}
