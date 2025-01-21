c50ac379d2a3631031734028e593aa07762360b9
motemotech <i.am.nicoshark@gmail.com>
2025-01-21 22:32:44 +0900

    fix build circuits

---
+++ circuits/scripts/build/build_register_circuits.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+source "scripts/build/common.sh"
+
+# Circuit-specific configurations
+CIRCUIT_TYPE="register"
+OUTPUT_DIR="build/${CIRCUIT_TYPE}"
+
+# Define circuits and their configurations
+# format: name:poweroftau:build_flag
+CIRCUITS=(
+    "register_sha1_sha1_sha1_ecdsa_brainpoolP224r1:21:false"
+    "register_sha1_sha1_sha1_ecdsa_secp256r1:21:false"
+    "register_sha1_sha1_sha1_rsa_65537_2048:20:false"
+    "register_sha1_sha256_sha256_rsa_65537_4096:20:false"
+    "register_sha256_sha224_sha224_ecdsa_brainpoolP224r1:21:false"
+    "register_sha256_sha256_sha256_ecdsa_brainpoolP224r1:21:false"
+    "register_sha256_sha256_sha256_ecdsa_brainpoolP256r1:21:false"
+    "register_sha256_sha256_sha256_ecdsa_secp256r1:21:false"
+    # Unable to build
+    "register_sha256_sha256_sha256_ecdsa_secp384r1:22:false"
+    "register_sha256_sha256_sha256_rsa_3_4096:20:false"
+    "register_sha256_sha256_sha256_rsa_65537_3072:20:false"
+    "register_sha256_sha256_sha256_rsa_65537_4096:20:false"
+    "register_sha256_sha256_sha256_rsapss_3_4096:21:false"
+    "register_sha256_sha256_sha256_rsapss_65537_4096:21:false"
+    "register_sha384_sha384_sha384_ecdsa_brainpoolP256r1:22:false"
+    # Unable to build
+    "register_sha384_sha384_sha384_ecdsa_brainpoolP384r1:22:false"
+    # Unable to build
+    "register_sha384_sha384_sha384_ecdsa_secp384r1:22:false"
+    "register_sha512_sha512_sha512_ecdsa_brainpoolP256r1:22:false"
+    # Unable to build
+    "register_sha512_sha512_sha512_ecdsa_brainpoolP384r1:22:false"
+    # Unable to build
+    "register_sha512_sha512_sha512_ecdsa_brainpoolP512r1:23:false"
+    "register_sha512_sha512_sha512_rsa_65537_4096:21:true"
+)
+
+build_circuits "$CIRCUIT_TYPE" "$OUTPUT_DIR" "${CIRCUITS[@]}" 
