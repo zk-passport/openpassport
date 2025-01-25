7a400b43a87be5b3a5deff6193e44470e53e1e0c
turnoffthiscomputer <colin.remi07@gmail.com>
2025-01-26 00:43:51 +0100

    fix imports in glue.test.ts

--- circuits/tests/others/glue.test.ts
+++ circuits/tests/others/glue.test.ts
@@ -1,26 +1,27 @@
 import { wasm as wasmTester } from 'circom_tester';
 import { describe, it } from 'mocha';
 import path from 'path';
-import { hexToDecimal, splitToWords } from '../../../common/src/utils/utils';
-import { formatInput } from '../../../common/src/utils/generateInputs';
 import { parseCertificate } from '../../../common/src/utils/certificate_parsing/parseCertificate';
 import { mock_dsc_secpk256_signed_with_rsa_sha1, mock_dsc_sha1_brainpoolP224r1, mock_dsc_sha384_brainpoolP384r1_384, mock_dsc_sha512_brainpoolP512r1 } from '../../../common/src/constants/mockCertificates';
+import { hexToDecimal } from '../../../common/src/utils/bytes';
+import { splitToWords } from '../../../common/src/utils/bytes';
+import { formatInput } from '../../../common/src/utils/circuits/generateInputs';
 
 const n_and_k = [
-    // { n: 120, k: 35, keyLength: 4096, isEcdsa: false },
+    { n: 120, k: 35, keyLength: 4096, isEcdsa: false },
     // { n: 64, k: 4, isEcdsa: true, dscCertificate: mock_dsc_secpk256_signed_with_rsa_sha1 },
     // { n: 64, k: 6, isEcdsa: true, dscCertificate: mock_dsc_sha384_brainpoolP384r1_384 },
     // { n: 64, k: 8, isEcdsa: true, dscCertificate: mock_dsc_sha512_brainpoolP512r1 },
     { n: 32, k: 7, isEcdsa: true, dscCertificate: mock_dsc_sha1_brainpoolP224r1 },
 
 
-    
+
 ]
 
-describe('Circuit Test', function () { 
+describe('Circuit Test', function () {
     this.timeout(0);
     n_and_k.forEach(({ n, k, keyLength, isEcdsa, dscCertificate }) => {
-        it(`n=${n},k=${k}`, async function () { 
+        it(`n=${n},k=${k}`, async function () {
 
             // Run circuit with inputs
             const circuit = await wasmTester(
@@ -37,24 +38,24 @@ describe('Circuit Test', function () {
                     ],
                 }
             );
-            
+
             let pubkey_dsc
             let pubkey_dsc_padded
 
-            if (isEcdsa) { 
+            if (isEcdsa) {
                 const {
                     publicKeyDetails,
                 } = parseCertificate(dscCertificate, 'dsc_cert');
                 const { bits, x, y } = publicKeyDetails;
-                
-                
+
+
                 const normalizedX = x.length % 2 === 0 ? x : '0' + x;
                 const normalizedY = y.length % 2 === 0 ? y : '0' + y;
 
                 const pubKey_dsc_x = splitToWords(BigInt(hexToDecimal(normalizedX)), n, k);
                 const pubKey_dsc_y = splitToWords(BigInt(hexToDecimal(normalizedY)), n, k);
                 pubkey_dsc = [...pubKey_dsc_x, ...pubKey_dsc_y];
-            
+
                 const fullPubKey = normalizedX + normalizedY;
                 pubkey_dsc_padded = splitToWords(BigInt(hexToDecimal(fullPubKey)), 8, 525);
             } else {
