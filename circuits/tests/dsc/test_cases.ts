f465e2ebe7e03bef1467f6fb2d28cb28e47a7605
0xturboblitz <florent.tavernier@gmail.com>
2025-01-28 17:26:38 -0800

    note which dsc test cases have no corresponding certificates

--- circuits/tests/dsc/test_cases.ts
+++ circuits/tests/dsc/test_cases.ts
@@ -9,7 +9,7 @@ export const sigAlgs = [
 
 export const fullSigAlgs = [
   { sigAlg: 'rsa', hashFunction: 'sha1', domainParameter: '65537', keyLength: '4096' },
-  { sigAlg: 'rsa', hashFunction: 'sha1', domainParameter: '3', keyLength: '4096' },
+  { sigAlg: 'rsa', hashFunction: 'sha1', domainParameter: '3', keyLength: '4096' }, // does't have a dsc
   { sigAlg: 'rsa', hashFunction: 'sha256', domainParameter: '65537', keyLength: '4096' },
   { sigAlg: 'rsa', hashFunction: 'sha384', domainParameter: '65537', keyLength: '4096' },
   { sigAlg: 'rsa', hashFunction: 'sha512', domainParameter: '65537', keyLength: '4096' },
@@ -18,11 +18,11 @@ export const fullSigAlgs = [
   { sigAlg: 'rsapss', hashFunction: 'sha256', saltLen: '32', domainParameter: '65537', keyLength: '2048' },
   { sigAlg: 'rsapss', hashFunction: 'sha256', saltLen: '32', domainParameter: '65537', keyLength: '3072' },
   { sigAlg: 'rsapss', hashFunction: 'sha256', saltLen: '32', domainParameter: '3', keyLength: '3072' },
-  { sigAlg: 'rsapss', hashFunction: 'sha256', saltLen: '32', domainParameter: '3', keyLength: '4096' },
+  { sigAlg: 'rsapss', hashFunction: 'sha256', saltLen: '32', domainParameter: '3', keyLength: '4096' }, // does't have a dsc
   { sigAlg: 'rsapss', hashFunction: 'sha384', saltLen: '48', domainParameter: '65537', keyLength: '3072' },
-  { sigAlg: 'rsapss', hashFunction: 'sha384', saltLen: '48', domainParameter: '65537', keyLength: '4096' },
+  { sigAlg: 'rsapss', hashFunction: 'sha384', saltLen: '48', domainParameter: '65537', keyLength: '4096' }, // does't have a dsc
   { sigAlg: 'ecdsa', hashFunction: 'sha1', domainParameter: 'secp256r1', keyLength: '256' },
-  { sigAlg: 'ecdsa', hashFunction: 'sha1', domainParameter: 'brainpoolP256r1', keyLength: '256' },
+  { sigAlg: 'ecdsa', hashFunction: 'sha1', domainParameter: 'brainpoolP256r1', keyLength: '256' }, // does't have a dsc
   { sigAlg: 'ecdsa', hashFunction: 'sha256', domainParameter: 'brainpoolP256r1', keyLength: '256' },
   { sigAlg: 'ecdsa', hashFunction: 'sha256', domainParameter: 'secp256r1', keyLength: '256' },
   { sigAlg: 'ecdsa', hashFunction: 'sha256', domainParameter: 'secp384r1', keyLength: '384' },
