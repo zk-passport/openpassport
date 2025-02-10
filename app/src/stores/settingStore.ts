31a9240dbf617760594b89c4673ffc059b7a01a7
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-02-10 18:19:38 +0100

    fix disclaimer notice reshowing after being dismissed (#62)

--- app/src/stores/settingStore.ts
+++ app/src/stores/settingStore.ts
@@ -1,13 +1,25 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 interface SettingsState {
   hasPrivacyNoteBeenDismissed: boolean;
   dismissPrivacyNote: () => void;
 }
 
-export const useSettingStore = create<SettingsState>(set => ({
-  hasPrivacyNoteBeenDismissed: false,
-  dismissPrivacyNote: () => {
-    set({ hasPrivacyNoteBeenDismissed: true });
-  },
-}));
+/*
+ * This store is used to store the settings of the app. Dont store anything sensative here
+ */
+export const useSettingStore = create<SettingsState>()(
+  persist(
+    (set, _get) => ({
+      hasPrivacyNoteBeenDismissed: false,
+      dismissPrivacyNote: () => set({ hasPrivacyNoteBeenDismissed: true }),
+    }),
+    {
+      name: 'setting-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+      onRehydrateStorage: () => console.log('Rehydrated settings'),
+    },
+  ),
+);
