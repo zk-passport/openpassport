0fdfef74da08f8fc7f1dcc7da063d00d951df201
Aaron DeRuvo <aaron.deruvo@clabs.co>
2025-02-19 17:07:28 +0100

    redirectable links (#166)

--- app/src/consts/links.ts
+++ app/src/consts/links.ts
@@ -5,14 +5,12 @@ export const termsUrl = 'https://self.xyz/terms';
 export const privacyUrl = 'https://self.xyz/privacy';
 
 // TODO REAL LINK
-export const telegramUrl = 'https://t.me/selfxyz-need-a-real-link';
+export const telegramUrl = 'https://self.xyz/telegram';
 
 export const gitHubUrl = 'https://github.com/selfxyz/self';
 
 // TODO real link
-export const appStoreUrl =
-  'https://apps.apple.com/us/app/self-xyz/id1581442024';
+export const appStoreUrl = 'https://self.xyz/app-store';
 
 // TODO real link
-export const playStoreUrl =
-  'https://play.google.com/store/apps/details?id=xyz.self.app';
+export const playStoreUrl = 'https://self.xyz/play-store';
