93a76cedc31a6d367c2c759043e84bcb558cd60f
turboblitz <florent.tavernier@gmail.com>
2025-02-19 18:22:30 -0800

    remove previous proof verifier (#176)

--- sdk/backend-api/src/contracts/infrastructure/contracts.controller.ts
+++ sdk/backend-api/src/contracts/infrastructure/contracts.controller.ts
@@ -1,5 +1,4 @@
 import Elysia, { t } from 'elysia';
-import { ProofVerifier } from '../../contracts/application/proofVerifier';
 import { RegistryContract } from '../../contracts/application/registryContract';
 import { HubContract } from '../application/hubContract';
 import { getChain } from '../../contracts/application/chains';
@@ -380,67 +379,6 @@ export const ContractsController = new Elysia()
         description: 'Retrieve the DSC Circuit Verifier address by passing id as a query parameter.',
       },
     }
-  )
-  .post(
-    'verify-vc-and-disclose-proof',
-    async (request) => {
-      try {
-        const registryContract = new RegistryContract(
-          getChain(process.env.NETWORK as string),
-          process.env.PRIVATE_KEY as `0x${string}`,
-          process.env.RPC_URL as string
-        );
-
-        const identityCommitmentRoot = await registryContract.getIdentityCommitmentMerkleRoot();
-        const ofacRoot = await registryContract.getOfacRoot();
-
-        const { proof, publicSignals } = request.body;
-
-        const proofVerifier = new ProofVerifier(
-          process.env.OFAC_ENABLED === "true",
-          process.env.OLDER_THAN_ENABLED === "true",
-          process.env.EXCLUDED_COUNTRIES_ENABLED === "true",
-          ofacRoot,
-          process.env.OLDER_THAN || "18",
-          (process.env.EXCLUDED_COUNTRIES || "USA,IRN,CHN").split(','),
-          identityCommitmentRoot,
-          {}
-        );
-
-        await proofVerifier.verifyVcAndDiscloseProof(proof, publicSignals);
-
-        return {
-          status: "success",
-          data: ["Valid VC and disclose proof"],
-        };
-      } catch (error) {
-        return {
-          status: "error",
-          message: error instanceof Error ? error.message : "Unknown error",
-        };
-      }
-    },
-    {
-      body: t.Object({
-        proof: t.Any(),
-        publicSignals: t.Any(),
-      }),
-      response: {
-        200: t.Object({
-          status: t.String(),
-          data: t.Array(t.String()),
-        }),
-        500: t.Object({
-          status: t.String(),
-          message: t.String(),
-        }),
-      },
-      detail: {
-        tags: ['Contracts'],
-        summary: 'Verify a VC and disclose a proof',
-        description: 'Verify a VC and disclose a proof',
-      },
-    },
   )
   .get(
     'dsc-commitment-tree',
