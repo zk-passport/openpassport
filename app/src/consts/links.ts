7a527998edf77c957d9fe6e6e4a105d7b4dac5b6
0xturboblitz <florent.tavernier@gmail.com>
2025-03-06 21:28:54 -0800

    update links

--- app/src/consts/links.ts
+++ app/src/consts/links.ts
@@ -4,13 +4,10 @@ export const termsUrl = 'https://self.xyz/terms';
 
 export const privacyUrl = 'https://self.xyz/privacy';
 
-// TODO REAL LINK
-export const telegramUrl = 'https://self.xyz/telegram';
+export const telegramUrl = 'https://t.me/self_xyz';
 
 export const gitHubUrl = 'https://github.com/selfxyz/self';
 
-// TODO real link
-export const appStoreUrl = 'https://self.xyz/app-store';
+export const appStoreUrl = 'https://apps.apple.com/app/self-zk/id6478563710';
 
-// TODO real link
-export const playStoreUrl = 'https://self.xyz/play-store';
+export const playStoreUrl = 'https://play.google.com/store/apps/details?id=com.proofofpassportapp';
